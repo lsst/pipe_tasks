9d0718962adf292dcb02d6fbca05e4792f74c65b
Paul Price <price@astro.princeton.edu>
2012-02-13 16:41:43 -0500

    Fix compile errors.

--- processCcd.py
+++ processCcd.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
+import lsst.pex.config as pexConfig
 from lsst.ip.isr import IsrTask
+import lsst.pipe.tasks.processCcd as ptProcessCcd
 from lsst.pipe.tasks.photometry import PhotometryTask
-
 import hsc.pipe.tasks.astrometry as hscAstrom
 import hsc.pipe.tasks.suprimecam as hscSuprimeCam
 import hsc.pipe.tasks.calibrate as hscCalibrate
 import hsc.pipe.tasks.hscDc2 as hscDc2
 
 class HscProcessCcdConfig(ptProcessCcd.ProcessCcdConfig):
-    calibrate = pexConfig.ConfigField(dtype=HscCalibrateTask.ConfigClass, doc="Calibration")
+    calibrate = pexConfig.ConfigField(dtype=hscCalibrate.HscCalibrateConfig, doc="Calibration")
 
-class SuprimeCamProcessCcd(ptProcessCcd.ProcessCcd):
+class SuprimeCamProcessCcdTask(ptProcessCcd.ProcessCcdTask):
     ConfigClass = HscProcessCcdConfig
     def __init__(self, config=HscProcessCcdConfig(), *args, **kwargs):
         pipeBase.Task.__init__(self, config=config, *args, **kwargs)
@@ -19,7 +20,7 @@ class SuprimeCamProcessCcd(ptProcessCcd.ProcessCcd):
         self.makeSubtask("calibrate", hscCalibrate.HscCalibrateTask, config=config.calibrate)
         self.makeSubtask("photometry", PhotometryTask, config=config.photometry)
 
-class HscDc2ProcessCcd(ptProcessCcd.ProcessCcd):
+class HscDc2ProcessCcdTask(ptProcessCcd.ProcessCcdTask):
     ConfigClass = HscProcessCcdConfig
     def __init__(self, config=HscProcessCcdConfig(), *args, **kwargs):
         pipeBase.Task.__init__(self, config=config, *args, **kwargs)
