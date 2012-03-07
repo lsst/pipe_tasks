d899a84c25fba0d01cb7f13244438bef016ade90
Robert Lupton the Good <rhl@astro.princeton.edu>
2012-03-07 10:31:56 -0500

    adjustBackground has moved to meas_utils

--- python/lsst/pipe/tasks/calibrate.py
+++ python/lsst/pipe/tasks/calibrate.py
@@ -95,11 +95,6 @@ class CalibrateConfig(pexConfig.Config):
         doc = "Subtract background (after computing it, if not supplied)?",
         default = True,
     )
-    adjustBackground = pexConfig.Field(
-        dtype = float,
-        doc = "Fiddle factor to add to the background; debugging only",
-        default = 0.0,
-    )
     background = pexConfig.ConfigField(
         dtype = muDetection.estimateBackground.ConfigClass,
         doc = "Background estimation configuration"
@@ -149,12 +144,6 @@ class CalibrateTask(pipeBase.Task):
                 bg, exposure = muDetection.estimateBackground(exposure, self.config.background, subtract=True)
                 del bg
 
-            if self.config.adjustBackground:
-                self.log.log(self.log.WARN, "Fiddling the background by %g" % self.config.adjustBackground)
-                mi = exposure.getMaskedImage()
-                mi -= self.config.adjustBackground
-                del mi
-
             self.display('background', exposure=exposure)
         
         if self.config.doPsf or self.config.doAstrometry or self.config.doZeropoint:
@@ -184,6 +173,7 @@ class CalibrateTask(pipeBase.Task):
                 background, exposure = muDetection.estimateBackground(
                     exposure, self.config.background, subtract=True)
                 self.log.log(self.log.INFO, "Fit and subtracted background")
+
             self.display('background', exposure=exposure)
 
         if self.config.doPsf and (self.config.doAstrometry or self.config.doZeropoint):
