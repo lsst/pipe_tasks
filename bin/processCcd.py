f11ebfe3d625e813cd3655a1c1a9f2b31cb7e65d
Russell Owen <rowen@uw.edu>
2012-04-26 16:03:47 -0700

    First cut

--- bin/processCcd.py
+++ bin/processCcd.py
@@ -20,6 +20,6 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-from lsst.pipe.tasks.processCcdLsstSim import ProcessCcdLsstSimTask
+from lsst.pipe.tasks.processCcdLsstSim import ProcessCcdTask
 
-ProcessCcdLsstSimTask.parseAndRun()
+ProcessCcdTask.parseAndRun()
