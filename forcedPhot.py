c7b8eb532a64c7cc7ab29de3d4595dbd9b731640
Paul Price <price@astro.princeton.edu>
2012-05-01 05:55:50 +0900

    Got forced photometry working\!

--- forcedPhot.py
+++ forcedPhot.py
@@ -6,7 +6,9 @@ from lsst.pex.config import Config, Field, ConfigField
 from lsst.pipe.base import Task
 
 import lsst.daf.base as dafBase
+import lsst.afw.detection as afwDet
 import lsst.afw.table as afwTable
+import lsst.ip.isr as ipIsr
 import lsst.meas.algorithms as measAlg
 import lsst.pipe.tasks.calibrate as ptCal
 
@@ -16,6 +18,11 @@ class ForcedPhotConfig(Config):
     detection = ConfigField(dtype=measAlg.SourceDetectionConfig, doc="Configuration for detection on stack")
     measurement = ConfigField(dtype=measAlg.SourceMeasurementConfig,
                               doc="Configuration for measurement on CCD")
+
+    def setDefaults(self):
+        self.calibrate.doAstrometry = False
+        self.calibrate.doPhotoCal = False
+
 
 class ForcedPhotTask(Task):
     ConfigClass = ForcedPhotConfig
@@ -32,14 +39,19 @@ class ForcedPhotTask(Task):
 
     def run(self, butler, stackId, expIdList):
         stack = butler.get("stack", stackId)
+        self.interpolateNans(stack, afwDet.createPsf("DoubleGaussian", 15, 15, 1.0))       
+
+#        import pdb;pdb.set_trace()
+        
         calib = self.calibrate.run(stack)
         stack = calib.exposure
         stackWcs = stack.getWcs()
-        
+
         det = self.detect(stack)
         stackSources = det.sources
         self.measurement.run(stack, stackSources, apCorr=calib.apCorr)
-        butler.put(stackSources, "stacksrc", stackId)
+        butler.put(stackSources, "stacksources", stackId)
+        butler.put(stack.getPsf(), "stackpsf", stackId)
 
         del det
         del calib
@@ -53,9 +65,12 @@ class ForcedPhotTask(Task):
             wcs = exposure.getWcs()
             apCorr = butler.get("apCorr", expId)
 
+            # XXX only bother with sources that are definitely on the image?
             sources = stackSources.copy()
+
             self.measurement.run(exposure, sources, apCorr=apCorr, references=stackSources, refWcs=stackWcs)
-            butler.put("src", sources)
+            butler.put(sources, "src", expId)
+
             del exposure
             del psf
             del wcs
@@ -63,6 +78,14 @@ class ForcedPhotTask(Task):
             del sources
             gc.collect()
 
+    def interpolateNans(self, exposure, psf):
+        exposure.getMaskedImage().getMask().addMaskPlane("UNMASKEDNAN")
+        nanMasker = ipIsr.UnmaskedNanCounterF()
+        nanMasker.apply(exposure.getMaskedImage())
+        nans = ipIsr.Isr().getDefectListFromMask(exposure.getMaskedImage(), maskName='UNMASKEDNAN')
+        self.log.log(self.log.INFO, "Interpolating over %d NANs" % len(nans))
+        measAlg.interpolateOverDefects(exposure.getMaskedImage(), psf, nans, 0.0)
+        
 
     def detect(self, exposure):
         table = afwTable.SourceTable.make(self.schema)
