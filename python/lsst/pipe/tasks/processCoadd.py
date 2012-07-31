9c9c4d2aef51581d1ec95c9f222cc3b5b5bd627e
Jim Bosch <jbosch@astro.princeton.edu>
2012-07-31 17:11:25 +0900

    refactored processCcd and processCoadd to remove duplicate coadd; now both inherit from processImage

--- python/lsst/pipe/tasks/processCoadd.py
+++ python/lsst/pipe/tasks/processCoadd.py
@@ -21,6 +21,7 @@
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
 import numpy as num
+
 import lsst.pex.config as pexConfig
 import lsst.pipe.base as pipeBase
 import lsst.daf.base as dafBase
@@ -31,66 +32,27 @@ import lsst.afw.math as afwMath
 from lsst.meas.algorithms import SourceDetectionTask, SourceMeasurementTask, SourceDeblendTask
 from lsst.pipe.tasks.calibrate import CalibrateTask
 from .coadd import CoaddArgumentParser
+from .processImage import ProcessImageTask
 
-class ProcessCoaddConfig(pexConfig.Config):
+class ProcessCoaddConfig(ProcessImageTask.ConfigClass):
     """Config for ProcessCoadd"""
     coaddName = pexConfig.Field(
         doc = "coadd name: typically one of deep or goodSeeing",
         dtype = str,
         default = "deep",
     )
-    doScaleVariance = pexConfig.Field(dtype=bool, default=True, doc = "Scale variance plane using empirical noise") 
+    doScaleVariance = pexConfig.Field(dtype=bool, default=True, doc = "Scale variance plane using empirical noise")
 
-    doCalibrate = pexConfig.Field(dtype=bool, default=True, doc = "Perform calibration?")
-    doDetection = pexConfig.Field(dtype=bool, default=True, doc = "Detect sources?")
-    ## NOTE, default this to False until it is fully vetted; #2138
-    doDeblend = pexConfig.Field(dtype=bool, default=False, doc = "Deblend sources?")
-    doMeasurement = pexConfig.Field(dtype=bool, default=True, doc = "Measure sources?")
-    doWriteCalibrate = pexConfig.Field(dtype=bool, default=True, doc = "Write calibration results?")
-    doWriteSources = pexConfig.Field(dtype=bool, default=True, doc = "Write sources?")
-    doWriteHeavyFootprintsInSources = pexConfig.Field(dtype=bool, default=False,
-                                                      doc = "Include HeavyFootprint data in source table?")
-
-    calibrate = pexConfig.ConfigurableField(
-        target = CalibrateTask,
-        doc = "Calibration (inc. high-threshold detection and measurement)",
-    )
-    detection = pexConfig.ConfigurableField(
-        target = SourceDetectionTask,
-        doc = "Low-threshold detection for final measurement",
-    )
-    deblend = pexConfig.ConfigurableField(
-        target = SourceDeblendTask,
-        doc = "Split blended sources into their components",
-    )
-    measurement = pexConfig.ConfigurableField(
-        target = SourceMeasurementTask,
-        doc = "Final source measurement on low-threshold detections",
-    )
-
-    def validate(self):
-        pexConfig.Config.validate(self)
-        if self.doMeasurement and not self.doDetection:
-            raise ValueError("Cannot run source measurement without source detection.")
-
-class ProcessCoaddTask(pipeBase.CmdLineTask):
-    """Process a CCD for SDSS Coadd
+class ProcessCoaddTask(ProcessImageTask.ConfigClass):
+    """Process a Coadd image
     
     """
     ConfigClass = ProcessCoaddConfig
     _DefaultName = "processCoadd"
 
     def __init__(self, **kwargs):
-        pipeBase.CmdLineTask.__init__(self, **kwargs)
-        self.makeSubtask("calibrate")
-        self.schema = afwTable.SourceTable.makeMinimalSchema()
-        self.algMetadata = dafBase.PropertyList()
-        if self.config.doDetection:
-            self.makeSubtask("detection", schema=self.schema)
-        if self.config.doDeblend:
-            self.makeSubtask("deblend", schema=self.schema)
-        if self.config.doMeasurement:
-            self.makeSubtask("measurement", schema=self.schema, algMetadata=self.algMetadata)
+        ProcessImageTask.__init__(self, **kwargs)
+        self.dataPrefix = self.coaddName + "Coadd_"
 
     @pipeBase.timeMethod
     def scaleVariance(self, exposure):
@@ -104,28 +66,26 @@ class ProcessCoaddTask(pipeBase.CmdLineTask):
         self.log.info("Renormalising variance by %f" % (vrat))
         var   *= vrat
 
-    @pipeBase.timeMethod
-    def run(self, sensorRef, sources=None):
-        """Process a CCD: including source detection, photometry and WCS determination
+    def makeIdFactory(self, dataRef):
+        expBits = dataRef.get(self.config.coaddName + "CoaddId_bits")
+        expId = long(dataRef.get(self.config.coaddName + "CoaddId"))
+        return afwTable.IdFactory.makeSource(expId, 64 - expBits)
         
-        @param sensorRef: sensor-level butler data reference to SDSS coadd patch
+
+    @pipeBase.timeMethod
+    def run(self, dataRef):
+        """Process a coadd image
+        
+        @param dataRef: butler data reference corresponding to coadd patch
         @return pipe_base Struct containing these fields:
         - exposure: calibrated exposure (calexp): as computed if config.doCalibrate,
             else as upersisted and updated if config.doDetection, else None
         - calib: object returned by calibration process if config.doCalibrate, else None
         - apCorr: aperture correction: as computed config.doCalibrate, else as unpersisted
             if config.doMeasure, else None
-        - sources: detected source if config.doPhotometry, else None
+        - sources: detected source if config.doDetection, else None
         """
-        self.log.log(self.log.INFO, "Processing %s" % (sensorRef.dataId))
-        outPrefix = self.config.coaddName + "Coadd_"
-
-        # We make one IdFactory that will be used by both icSrc and src datasets;
-        # I don't know if this is the way we ultimately want to do things, but at least
-        # this ensures the source IDs are fully unique.
-        expBits = sensorRef.get(self.config.coaddName + "CoaddId_bits")
-        expId = long(sensorRef.get(self.config.coaddName + "CoaddId"))
-        idFactory = afwTable.IdFactory.makeSource(expId, 64 - expBits)
+        self.log.log(self.log.INFO, "Processing %s" % (dataRef.dataId))
 
         # initialize outputs
         calExposure = None
@@ -134,75 +94,22 @@ class ProcessCoaddTask(pipeBase.CmdLineTask):
         psf = None
 
         if self.config.doCalibrate:
-            self.log.log(self.log.INFO, "Performing Calibrate on coadd %s" % (sensorRef.dataId))
-            coadd = sensorRef.get(self.config.coaddName+"Coadd")
+            coadd = dataRef.get(self.config.coaddName+"Coadd")
             initPsfName = outPrefix + "initPsf"
-            if sensorRef.datasetExists(initPsfName):
-                initPsf = sensorRef.get(initPsfName)
+            if dataRef.datasetExists(self.dataPrefix + "initPsf"):
+                initPsf = dataRef.get(self.dataPrefix + "initPsf")
                 coadd.setPsf(initPsf)
             else:
                 self.log.warn("Could not load initial PSF; dataset does not exist")
             if self.config.doScaleVariance:
                 self.scaleVariance(coadd)
+        else:
+            coadd = None
 
-            calib = self.calibrate.run(coadd, idFactory=idFactory)
-            calExposure = calib.exposure
-            apCorr = calib.apCorr
-            if self.config.doWriteCalibrate:
-                sensorRef.put(calib.sources, outPrefix+"icSrc")
-                if calib.psf is not None:
-                    sensorRef.put(calib.psf, outPrefix+"psf")
-                if calib.apCorr is not None:
-                    sensorRef.put(calib.apCorr, outPrefix+"apCorr")
-                if calib.matches is not None:
-                    normalizedMatches = afwTable.packMatches(calib.matches)
-                    normalizedMatches.table.setMetadata(calib.matchMeta)
-                    sensorRef.put(normalizedMatches, outPrefix+"icMatch")
-
-        if self.config.doDetection:
-            if calExposure is None:
-                calexpName = outPrefix+"calexp"
-                if not sensorRef.datasetExists(calexpName):
-                    raise pipeBase.TaskError("doCalibrate false, doDetection true and %s does not exist" % \
-                        (calexpName,))
-                calExposure = sensorRef.get(calexpName)
-            table = afwTable.SourceTable.make(self.schema, idFactory)
-            table.setMetadata(self.algMetadata)
-            sources = self.detection.makeSourceCatalog(table, calExposure).sources
-
-        if self.config.doWriteCalibrate:
-            # wait until after detection, since that sets detected mask bits and may tweak the background;
-            # note that this overwrites an existing calexp if doCalibrate false
-            if calExposure is None:
-                self.log.log(self.log.WARN, "calibrated exposure is None; cannot save it")
-            else:
-                sensorRef.put(calExposure, outPrefix+"calexp")
-
-        if self.config.doDeblend:
-            if calExposure is None:
-                calExposure = sensorRef.get(outPrefix + 'calexp')
-            if psf is None:
-                psf = sensorRef.get(outPrefix + 'psf')
-
-            assert(calExposure)
-            assert(psf)
-            assert(sources)
-            self.deblend.run(calExposure, sources, psf)
-
-        if self.config.doMeasurement:
-            if calib is None:
-                apCorr = sensorRef.get(outPrefix+"apCorr")
-            self.measurement.run(calExposure, sources, apCorr)
-
-        if self.config.doWriteSources:
-            sensorRef.put(sources, outPrefix+"src")
-
-        return pipeBase.Struct(
-            exposure = calExposure,
-            calib = calib,
-            apCorr = apCorr,
-            sources = sources,
-        )
+        # delegate most of the work to ProcessImageTask
+        result = self.process(dataRef, postIsrExposure)
+        result.coadd = coadd
+        return result
 
     @classmethod
     def _makeArgumentParser(cls):
