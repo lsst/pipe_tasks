9e6e88851a27a783f0dc3fe1e1f2c44543a2ba30
Jim Bosch <jbosch@astro.princeton.edu>
2013-04-12 15:40:47 -0400

    Complete rewrite of forced photometry tasks.
    Forced photometry tasks now get references from pipe_task-style
    coadds (instead of hscMosaic-style "stacks"), and can be run on both CCD images and coadds.

---
+++ bin/forcedPhotCcd.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+from lsst.pipe.tasks.forcedPhotCcd import ForcedPhotCcdTask
+ForcedPhotCcdTask.parseAndRun()
---
+++ python/lsst/pipe/tasks/forcedPhotCcd.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+#
+# LSST Data Management System
+# Copyright 2008-2013 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+
+import argparse
+
+import lsst.afw.table
+import lsst.afw.image
+import lsst.pipe.base
+from lsst.pex.config import Config, ConfigurableField, DictField, Field
+from .forcedPhotImage import ForcedPhotImageTask
+
+try:
+    from lsst.meas.mosaic import applyMosaicResults
+except ImportError:
+    applyMosaicResults = None
+
+__all__ = ("ForcedPhotCcdTask",)
+
+class CcdForcedSrcDataIdContainer(lsst.pipe.base.DataIdContainer):
+    """A version of lsst.pipe.base.DataIdContainer specialized for forced photometry on CCDs.
+    
+    Required because we need to add "tract" to the raw data ID keys, and that's tricky.
+    """
+    def makeDataRefList(self, namespace):
+        """Make self.refList from self.idList
+        """
+        for dataId in self.idList:
+            if "tract" not in dataId:
+                raise argparse.ArgumentError(None, "--id must include tract")
+            tract = dataId.pop("tract")
+            # making a DataRef for src fills out any missing keys
+            srcDataRef = namespace.butler.dataRef("src", dataId=dataId)
+            dataId.update(srcDataRef.dataId, tract=tract)
+            dataRef = namespace.butler.dataRef(
+                datasetType = "forced_src",
+                dataId = dataId,
+            )
+            self.refList.append(dataRef)
+
+class ForcedPhotCcdConfig(ForcedPhotImageTask.ConfigClass):
+    doApplyUberCal = Field(
+        dtype = bool,
+        doc = "Apply meas_mosaic ubercal results to input calexps?",
+        default = True
+    )
+
+class ForcedPhotCcdTask(ForcedPhotImageTask):
+    """Run forced measurement on CCD images
+    """
+
+    ConfigClass = ForcedPhotCcdConfig
+    _DefaultName = "forcedPhotCcd"
+    dataPrefix = ""  # Name to prepend to all input and output datasets (e.g. 'goodSeeingCoadd_')
+
+    def makeIdFactory(self, dataRef):
+        expBits = dataRef.get("ccdExposureId_bits")
+        expId = long(dataRef.get("ccdExposureId"))
+        return lsst.afw.table.IdFactory.makeSource(expId, 64 - expBits)        
+
+    def fetchReferences(self, dataRef, exposure):
+        return self.references.fetchInBox(dataRef, exposure.getBBox(lsst.afw.image.PARENT), exposure.getWcs())
+
+    def getExposure(self, dataRef):
+        """Read input exposure to measure
+
+        @param dataRef       Data reference from butler
+        """
+        exposure = dataRef.get("calexp", immediate=True)
+        if not self.config.doApplyUberCal:
+            return exposure
+        if applyMosaicResults is None:
+            raise RuntimeError(
+                "Cannot use improved calibrations for %s because meas_mosaic could not be imported."
+                % dataRef.dataId
+                )
+        else:
+            applyMosaicResults(dataRef, calexp=exposure)
+        return exposure
+
+    @classmethod
+    def _makeArgumentParser(cls):
+        parser = lsst.pipe.base.ArgumentParser(name=cls._DefaultName)
+        parser.add_id_argument("--id", "forced_src", help="data ID, with raw CCD keys + tract",
+                               ContainerClass=CcdForcedSrcDataIdContainer)
+        return parser
