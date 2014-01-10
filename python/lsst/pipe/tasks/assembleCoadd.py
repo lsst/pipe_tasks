d01c3fe8694e7491acdc67e82ddcc0176bb66701
Paul Price <price@astro.princeton.edu>
2014-01-10 21:50:20 +0000

    Tone down some coadd chatter.
    Don't care exactly what sub-region is being stacked.
    
    Ported from unticketed 565af42b6 on HSC's hscPipe.

--- python/lsst/pipe/tasks/assembleCoadd.py
+++ python/lsst/pipe/tasks/assembleCoadd.py
@@ -453,7 +453,7 @@ class AssembleCoaddTask(CoaddBaseTask):
         @param statsFlags: Statistic for coadd
         @param statsCtrl: Statistics control object for coadd
         """
-        self.log.info("Computing coadd over %s" % bbox)
+        self.log.logdebug("Computing coadd over %s" % bbox)
         tempExpName = self.getTempExpDatasetName()
         coaddMaskedImage = coaddExposure.getMaskedImage()
         coaddView = afwImage.MaskedImageF(coaddMaskedImage, bbox, afwImage.PARENT, False)
