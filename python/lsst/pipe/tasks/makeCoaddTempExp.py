a5cea3e107b3d11ad3c53ebca615ededc801aa5e
Russell Owen <rowen@uw.edu>
2014-03-13 11:42:51 -0700

    Minor cleanups in response to Andy B's review

--- python/lsst/pipe/tasks/makeCoaddTempExp.py
+++ python/lsst/pipe/tasks/makeCoaddTempExp.py
@@ -171,7 +171,8 @@ class MakeCoaddTempExpTask(CoaddBaseTask):
                     coaddTempExp.setCalib(exposure.getCalib())
                     coaddTempExp.setFilter(exposure.getFilter())
                     didSetMetadata = True
-            except Exception:
+            except Exception, e:
+                self.log.warn("Error processing calexp %s; skipping it: %s" % (calExpRef.dataId, e))
                 continue
             inputRecorder.addCalExp(calExp, ccdId, numGoodPix)
 
