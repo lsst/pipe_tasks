79013ddbdef8baa02cf0f8995811d92cc97266b1
pgee <pgee@pgeepc2.physics.ucdavis.edu>
2014-06-24 08:08:38 -0700

    Divided the SourceCatalog for calibrate into two pieces, and reassembled it at the end of run(), so prefix not required for measurement.

--- python/lsst/pipe/tasks/calibrate.py
+++ python/lsst/pipe/tasks/calibrate.py
@@ -101,18 +101,14 @@ class CalibrateConfig(pexConfig.Config):
 
     def validate(self):
         pexConfig.Config.validate(self)
-        if self.doPsf and (self.doPhotoCal or self.doAstrometry):
-            if self.initialMeasurement.prefix == self.measurement.prefix:
-                raise ValueError("CalibrateConfig.initialMeasurement and CalibrateConfig.measurement "\
-                                     "have the same prefix; field names may clash.")
+
         if self.doPhotoCal and not self.doAstrometry:
             raise ValueError("Cannot do photometric calibration without doing astrometric matching")
 
     def setDefaults(self):
         self.detection.includeThresholdMultiplier = 10.0
-        self.initialMeasurement.prefix = "initial."
         self.initialMeasurement.algorithms.names -= ["correctfluxes", "classification"]
-        initflags = [self.initialMeasurement.prefix+x for x in self.measurePsf.starSelector["catalog"].badStarPixelFlags]
+        initflags = [x for x in self.measurePsf.starSelector["catalog"].badStarPixelFlags]
         self.measurePsf.starSelector["catalog"].badStarPixelFlags.extend(initflags)
         self.background.binSize = 1024        
 
@@ -327,16 +323,47 @@ into your debug.py file and run calibrateTask.py with the \c --debug flag.
         """!Create the calibration task.  See CalibrateTask.init for documentation
         """
         pipeBase.Task.__init__(self, **kwargs)
-        self.schema = afwTable.SourceTable.makeMinimalSchema()
+        self.schema1 = afwTable.SourceTable.makeMinimalSchema()
+        minimalCount = self.schema1.getFieldCount()
         self.algMetadata = dafBase.PropertyList()
         self.tableVersion = tableVersion
         self.makeSubtask("repair")
-        self.makeSubtask("detection", schema=self.schema, tableVersion=tableVersion)
-        self.makeSubtask("initialMeasurement", schema=self.schema, algMetadata=self.algMetadata)
-        self.makeSubtask("measurePsf", schema=self.schema, tableVersion=tableVersion)
-        self.makeSubtask("measurement", schema=self.schema, algMetadata=self.algMetadata)
-        self.makeSubtask("astrometry", schema=self.schema, tableVersion=tableVersion)
-        self.makeSubtask("photocal", schema=self.schema, tableVersion=tableVersion)
+        self.makeSubtask("detection", schema=self.schema1, tableVersion=tableVersion)
+        beginInitial = self.schema1.getFieldCount()
+        self.makeSubtask("initialMeasurement", schema=self.schema1, algMetadata=self.algMetadata)
+        endInitial = self.schema1.getFieldCount()
+        self.makeSubtask("measurePsf", schema=self.schema1, tableVersion=tableVersion)
+
+        self.schema2 = afwTable.SourceTable.makeMinimalSchema()
+        beginMeasurement = self.schema2.getFieldCount()
+        self.makeSubtask("measurement", schema=self.schema2, algMetadata=self.algMetadata)
+        endMeasurement = self.schema2.getFieldCount()
+        self.makeSubtask("astrometry", schema=self.schema2, tableVersion=tableVersion)
+        self.makeSubtask("photocal", schema=self.schema2, tableVersion=tableVersion)
+        self.schema = afwTable.Schema()
+        self.schemaMapper1 = afwTable.SchemaMapper(self.schema1)
+        self.schemaMapper2 = afwTable.SchemaMapper(self.schema2)
+        if self.tableVersion == 0: separator = "."
+        else: separator =  "_"
+        count = 0
+        for item in self.schema1:
+            count = count + 1
+            field = item.getField()
+            name = field.getName()
+            if count > beginInitial and count <= endInitial: 
+                name = "initial" + separator + name 
+            self.schemaMapper1.addMapping(item.key, name)
+            self.schemaMapper2.addOutputField(field.copyRenamed(name))
+        count = 0
+        for item in self.schema2:
+            count = count + 1
+            if count <= minimalCount: continue
+            field = item.getField()
+            name = field.getName()
+            if count > beginMeasurement and count <= endMeasurement: name = "measurement" + separator + name 
+            self.schemaMapper2.addMapping(item.key, name)
+            self.schemaMapper1.addOutputField(field.copyRenamed(name))
+        self.schema = self.schemaMapper2.getOutputSchema()
 
     def getCalibKeys(self):
         """!
@@ -383,25 +410,33 @@ into your debug.py file and run calibrateTask.py with the \c --debug flag.
                 backgrounds.append(bg)
 
             self.display('background', exposure=exposure)
-        table = afwTable.SourceTable.make(self.schema, idFactory)
-        table.setMetadata(self.algMetadata)
-        table.setVersion(self.tableVersion)
-        detRet = self.detection.makeSourceCatalog(table, exposure)
-        sources = detRet.sources
+        table1 = afwTable.SourceTable.make(self.schema1, idFactory)
+        table1.setMetadata(self.algMetadata)
+        table1.setVersion(self.tableVersion)
+        detRet = self.detection.makeSourceCatalog(table1, exposure)
+        sources1 = detRet.sources
+        if self.config.doAstrometry or self.config.doPhotoCal:
+            table2 = afwTable.SourceTable.make(self.schema2, idFactory)
+            table2.setMetadata(self.algMetadata)
+            table2.setVersion(self.tableVersion)
+            sources2 = afwTable.SourceCatalog(table2)
+            table2.preallocate(detRet.fpSets.numPos) # not required, but nice
+            detRet.fpSets.positive.makeSources(sources2)
+
         if detRet.fpSets.background:
             backgrounds.append(detRet.fpSets.background)
 
         if self.config.doPsf:
-            self.initialMeasurement.measure(exposure, sources)
+            self.initialMeasurement.measure(exposure, sources1)
 
             if self.config.doAstrometry:
-                astromRet = self.astrometry.run(exposure, sources)
+                astromRet = self.astrometry.run(exposure, sources1)
                 matches = astromRet.matches
             else:
                 # If doAstrometry is False, we force the Star Selector to either make them itself
                 # or hope it doesn't need them.
                 matches = None
-            psfRet = self.measurePsf.run(exposure, sources, matches=matches)
+            psfRet = self.measurePsf.run(exposure, sources1, matches=matches)
             cellSet = psfRet.cellSet
             psf = psfRet.psf
         elif exposure.hasPsf():
@@ -431,10 +466,10 @@ into your debug.py file and run calibrateTask.py with the \c --debug flag.
             self.display('PSF_background', exposure=exposure)
 
         if self.config.doAstrometry or self.config.doPhotoCal:
-            self.measurement.run(exposure, sources)
+            self.measurement.run(exposure, sources2)
 
         if self.config.doAstrometry:
-            astromRet = self.astrometry.run(exposure, sources)
+            astromRet = self.astrometry.run(exposure, sources2)
             matches = astromRet.matches
             matchMeta = astromRet.matchMeta
         else:
@@ -468,8 +503,38 @@ into your debug.py file and run calibrateTask.py with the \c --debug flag.
         else:
             photocalRet = None
 
-        self.display('calibrate', exposure=exposure, sources=sources, matches=matches)
+        self.display('calibrate', exposure=exposure, sources=sources2, matches=matches)
+        assert(len(sources1) == len(sources2))
+        for i in range(len(sources1)):
+            foot2 = sources2[i].getFootprint()
+            foot = sources1[i].getFootprint()
+            assert(foot.getCentroid() == foot2.getCentroid())
+            spans = foot.getSpans()
+            spans2 = foot2.getSpans()
+            assert(len(spans) == len(spans2))
+            for j in range(len(spans)):
+                assert(spans[j] == spans2[j])
+        # now make the final sources catalog
+        table = afwTable.SourceTable.make(self.schemaMapper1.getOutputSchema(), idFactory)
+        table.setMetadata(self.algMetadata)
+        table.setVersion(self.tableVersion)
+        table.preallocate(len(sources1)) # not required, but nice
+        sources = afwTable.SourceCatalog(table)
+        sources.extend(sources1, self.schemaMapper1)
+        assert(len(sources) == len(sources2))
+        for i in range(len(sources)):
+            foot2 = sources2[i].getFootprint()
+            foot = sources[i].getFootprint()
+            assert(foot.getCentroid() == foot2.getCentroid())
+            spans = foot.getSpans()
+            spans2 = foot2.getSpans()
+            assert(len(spans) == len(spans2))
+            for j in range(len(spans)):
+                assert(spans[j] == spans2[j])
 
+        for i in range(len(sources)):
+            sources[i].assign(sources2[i], self.schemaMapper2)
+      
         return pipeBase.Struct(
             exposure = exposure,
             backgrounds = backgrounds,
