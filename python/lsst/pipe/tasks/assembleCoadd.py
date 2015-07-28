55f07ab0fe6cc5eea2949b512237d4127433ca75
John Swinbank <swinbank@princeton.edu>
2015-07-28 12:38:09 -0400

    fixup! WIP on matching with HSC.

--- python/lsst/pipe/tasks/assembleCoadd.py
+++ python/lsst/pipe/tasks/assembleCoadd.py
@@ -137,31 +137,29 @@ class AssembleCoaddTask(CoaddBaseTask):
 
         The coadd is computed as a mean with optional outlier rejection.
 
-        assembleCoaddTask only works on the dataset type 'coaddTempExp', which are 'coadd temp exposures'.
-        Each coaddTempExp is the size of a patch and contains data for one run, visit or
-        (for a non-mosaic camera it will contain data for a single exposure).
+        AssumeCoaddTask performs coaddition of "coadd temporary exposures" ("coaddTempExps").  Each
+        coaddTempExp is the size of a patch and contains data for one run, visit or (for a non-mosaic camera)
+        exposure. The coaddTempExps to coadd are selected from the provided selectDataList based on their
+        overlap with the patch specified by dataRef.
 
-        coaddTempExps, by default, will have backgrounds in them and will require
-        config.doMatchBackgrounds=True. However, makeCoaddTempExp.py can optionally create background-
-        subtracted coaddTempExps which can be coadded here by setting config.doMatchBackgrounds=False.
+        By default, coaddTempExps contain backgrounds and hence require config.doMatchBackgrounds=True.
+        Background-subtracted coaddTempExps can be coadded by setting config.doMatchBackgrounds=False.
 
-        Note the semantics of the dataRef parameter. If background matching is enabled
-        (config.doMatchBackgrounds=True) and automatic reference selection is disabled
-        (config.autoReference=False), then we require a coadd temp exposure (dataType 'Coadd_tempExp') to
-        serve as the reference. If this is not the case, we can use a coadd patch (datatype ('Coadd') which
-        simply defines the patch which is being co-added.
+        When background matching is enabled, the task may be configured to automatically select a reference
+        exposure (config.autoReference=True). If this is not done, then we require that the input dataRef
+        provides access to a coaddTempExp (dataset type coaddName + 'Coadd_tempExp') which is used as the
+        reference exposure.
 
-        @param dataRef: data reference for a coadd patch ('Coadd') OR a data reference
-        for a coadd temp exposure (of dataType 'Coadd_tempExp') which serves as the reference visit
-        if config.doMatchBackgrounds true and config.autoReference false)
-        If supplying a coadd patch: Must include keys "tract", "patch",
-            plus the camera-specific filter key (e.g. "filter")
-        Used to access the following data products (depending on the config):
-        - [in] self.config.coaddName + "Coadd_tempExp"
-        - [out] self.config.coaddName + "Coadd"
+        @param dataRef: Data reference defining the patch for coaddition and the reference coaddTempExp
+                        (if config.autoReference=False). Used to access the following data products:
+                        - [in] self.config.coaddName + "Coadd_skyMap"
+                        - [in] self.config.coaddName + "Coadd_tempExp" (optionally)
+                        - [out] self.config.coaddName + "Coadd"
+        @param selectDataList[in]: List of data references to coaddTempExp. Data to be coadded will be
+                                   selected from this list based on overlap with the patch defined by dataRef.
 
         @return: a pipeBase.Struct with fields:
-        - coaddExposure: coadd exposure
+                 - coaddExposure: coadd exposure
         """
         skyInfo = self.getSkyInfo(dataRef)
         calExpRefList = self.selectExposures(dataRef, skyInfo, selectDataList=selectDataList)
