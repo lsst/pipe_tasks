f5836d2302f3ed3bd6df946b104fe2b947890396
Jim Bosch <jbosch@astro.princeton.edu>
2016-06-24 22:29:12 +0200

    Test running ProcessCcd subtasks from Python.
    We just verify that these generate the same outputs as they do
    when run from the command-line, so it's really just a test that
    they don't raise exceptions.

--- tests/testProcessCcd.py
+++ tests/testProcessCcd.py
@@ -35,6 +35,7 @@ import numpy as np
 import lsst.utils
 import lsst.afw.geom as afwGeom
 import lsst.utils.tests
+from lsst.daf.persistence import Butler
 from lsst.ip.isr import IsrTask  # we assume obs_test uses base IsrTask here; may change in future.
 from lsst.pipe.tasks.characterizeImage import CharacterizeImageTask
 from lsst.pipe.tasks.calibrate import CalibrateTask
@@ -45,6 +46,18 @@ InputDir = os.path.join(obsTestDir, "data", "input")
 
 OutputName = None  # specify a name (as a string) to save the output repository
 
+
+def getObsTestConfig(TaskClass):
+    """Helper function to get a command-line task config customized by obs_test.
+
+    This duplicates the config override code in pipe_base's ArgumentParser, but
+    essentially in order to test it.
+    """
+    config = TaskClass.ConfigClass()
+    filename = os.path.join(obsTestDir, "config", TaskClass._DefaultName + ".py")
+    if os.path.exists(filename):
+        config.load(filename)
+    return config
 
 class ProcessCcdTestCase(lsst.utils.tests.TestCase):
 
@@ -158,7 +171,7 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
             else:
                 print("testProcessCcd.py's output data saved to %r" % (OutputName,))
 
-    def assertCatalogsEqual(self, catalog1, catalog2):
+    def assertCatalogsEqual(self, catalog1, catalog2, skipCols=()):
         """Compare two Catalogs for equality.
 
         This should only be used in contexts where it's unlikely that the catalogs will be subtly different;
@@ -172,7 +185,11 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
         fixNaN = lambda x: x if x == x else "NaN"
         for record1, record2 in zip(catalog1, catalog2):
             for name, item in d.iteritems():
-                self.assertEqual(fixNaN(record1.get(item.key)), fixNaN(record2.get(item.key)))
+                if name not in skipCols:
+                    self.assertEqual(
+                        fixNaN(record1.get(item.key)), fixNaN(record2.get(item.key)),
+                        "{} != {} in field {}".format(record1.get(item.key), record2.get(item.key), name)
+                    )
 
     def assertBackgroundListsEqual(self, bkg1, bkg2):
         """Compare two BackgroundLists for equality.
@@ -188,12 +205,36 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
         self.assertImagesEqual(im1, im2)
 
     def testComponents(self):
-        """test that we can run the first-level subtasks of ProcessCcdTasks separately on the command-line.
+        """test that we can run the first-level subtasks of ProcessCcdTasks.
+
+        This tests that we can run these subtasks from the command-line independently (they're all
+        CmdLineTasks) as well as directly from Python (without giving them access to a Butler).
 
         Aside from verifying that no exceptions are raised, we simply tests that most persisted results are
-        present and equivalent to in-memory results.
+        present and equivalent to both in-memory results.
         """
         outPath = tempfile.mkdtemp() if OutputName is None else "{}-Components".format(OutputName)
+        # We'll use an input butler to get data for the tasks we call from Python, but we won't ever give it
+        # to those tasks.
+        inputButler = lsst.daf.persistence.Butler(InputDir)
+        # Construct task instances we can use directly from Python
+        isrTask = IsrTask(
+            config=getObsTestConfig(IsrTask),
+            name="isr2"
+        )
+        # If we ever enable astrometry and photocal in obs_test, we'll need to pass a refObjLoader to these
+        # tasks.  To maintain the spirit of these tests, we'd ideally have a LoadReferenceObjectsTask class
+        # that doesn't require a Butler.  If we don't, we should construct a butler-based on outside these
+        # task constructors and pass the LoadReferenceObjectsTask instance to the task constructors.
+        charImageTask = CharacterizeImageTask(
+            config=getObsTestConfig(CharacterizeImageTask),
+            name="charImage2"
+        )
+        calibrateTask = CalibrateTask(
+            config=getObsTestConfig(CalibrateTask),
+            name="calibrate2",
+            icSourceSchema=charImageTask.schema
+        )
         try:
             dataId = dict(visit=1)
             dataIdStrList = ["%s=%s" % (key, val) for key, val in dataId.iteritems()]
@@ -202,8 +243,26 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
                 args=[InputDir, "--output", outPath, "--clobber-config", "--doraise", "--id"] + dataIdStrList,
                 doReturnResults=True,
             )
+            # We'll just use the butler to get the original image and calibration frames; it's not clear
+            # extending the test coverage to include that is worth it.
+            dataRef = inputButler.dataRef("raw", dataId=dataId)
+            rawExposure = dataRef.get("raw", immediate=True)
+            isrData = isrTask.readIsrData(dataRef, rawExposure)
+            isrResult2 = isrTask.run(
+                rawExposure,
+                bias=isrData.bias,
+                linearizer=isrData.linearizer,
+                flat=isrData.flat,
+                defects=isrData.defects,
+                fringes=isrData.fringes,
+                bfKernel=isrData.bfKernel
+            )
             self.assertMaskedImagesEqual(
                 isrResult1.parsedCmd.butler.get("postISRCCD", dataId, immediate=True).getMaskedImage(),
+                isrResult1.resultList[0].result.exposure.getMaskedImage()
+            )
+            self.assertMaskedImagesEqual(
+                isrResult2.exposure.getMaskedImage(),
                 isrResult1.resultList[0].result.exposure.getMaskedImage()
             )
 
@@ -211,16 +270,30 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
                 args=[InputDir, "--output", outPath, "--clobber-config", "--doraise", "--id"] + dataIdStrList,
                 doReturnResults=True,
             )
+            icResult2 = charImageTask.characterize(isrResult2.exposure)
             self.assertMaskedImagesEqual(
                 icResult1.parsedCmd.butler.get("icExp", dataId, immediate=True).getMaskedImage(),
+                icResult1.resultList[0].result.exposure.getMaskedImage()
+            )
+            self.assertMaskedImagesEqual(
+                icResult2.exposure.getMaskedImage(),
                 icResult1.resultList[0].result.exposure.getMaskedImage()
             )
             self.assertCatalogsEqual(
                 icResult1.parsedCmd.butler.get("icSrc", dataId, immediate=True),
                 icResult1.resultList[0].result.sourceCat
             )
+            self.assertCatalogsEqual(
+                icResult2.sourceCat,
+                icResult1.resultList[0].result.sourceCat,
+                skipCols=("id", "parent")  # since we didn't want to pass in an ExposureIdInfo, IDs disagree
+            )
             self.assertBackgroundListsEqual(
                 icResult1.parsedCmd.butler.get("icExpBackground", dataId, immediate=True),
+                icResult1.resultList[0].result.background
+            )
+            self.assertBackgroundListsEqual(
+                icResult2.background,
                 icResult1.resultList[0].result.background
             )
 
@@ -228,16 +301,34 @@ class ProcessCcdTestCase(lsst.utils.tests.TestCase):
                 args=[InputDir, "--output", outPath, "--clobber-config", "--doraise", "--id"] + dataIdStrList,
                 doReturnResults=True,
             )
+            calResult2 = calibrateTask.calibrate(
+                icResult2.exposure,
+                background=icResult2.background,
+                icSourceCat=icResult2.sourceCat
+            )
             self.assertMaskedImagesEqual(
                 calResult1.parsedCmd.butler.get("calexp", dataId, immediate=True).getMaskedImage(),
+                calResult1.resultList[0].result.exposure.getMaskedImage()
+            )
+            self.assertMaskedImagesEqual(
+                calResult2.exposure.getMaskedImage(),
                 calResult1.resultList[0].result.exposure.getMaskedImage()
             )
             self.assertCatalogsEqual(
                 calResult1.parsedCmd.butler.get("src", dataId, immediate=True),
                 calResult1.resultList[0].result.sourceCat
             )
+            self.assertCatalogsEqual(
+                calResult2.sourceCat,
+                calResult1.resultList[0].result.sourceCat,
+                skipCols=("id", "parent")
+            )
             self.assertBackgroundListsEqual(
                 calResult1.parsedCmd.butler.get("calexpBackground", dataId, immediate=True),
+                calResult1.resultList[0].result.background
+            )
+            self.assertBackgroundListsEqual(
+                calResult2.background,
                 calResult1.resultList[0].result.background
             )
 
