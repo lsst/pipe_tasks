05c56c8d7a2b262957e04502a897117282d89971
Russell Owen <rowen@uw.edu>
2016-06-28 08:29:24 -0700

    Add missing from __future__ import
    Add missing
      from __future__ import absolute_import, division, print_function
    to all three index catalog reader modules

--- python/lsst/pipe/tasks/htmIndexer.py
+++ python/lsst/pipe/tasks/htmIndexer.py
@@ -19,6 +19,7 @@
 # the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
+from __future__ import absolute_import, division, print_function
 import esutil
 
 
