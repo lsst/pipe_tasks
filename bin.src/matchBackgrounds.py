cfb7953deb9bfe3d62939352e2b78ef598233e3f
Hsin-Fang Chiang <hchiang2@illinois.edu>
2016-09-26 15:44:51 -0500

    Remove a unused import

--- bin.src/matchBackgrounds.py
+++ bin.src/matchBackgrounds.py
@@ -20,7 +20,6 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-import lsst.pex.logging as pexLog
 from lsst.pipe.tasks.matchBackgrounds import MatchBackgroundsTask
 
 MatchBackgroundsTask.parseAndRun()
