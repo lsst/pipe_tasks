f89ff44448e0d3e369597b38892eee1b89e859ed
Jim Bosch <jbosch@astro.princeton.edu>
2017-12-14 10:02:53 -0500

    Update badMaskPlane defaults to match HSC configuration.

--- python/lsst/pipe/tasks/assembleCoadd.py
+++ python/lsst/pipe/tasks/assembleCoadd.py
@@ -136,7 +136,7 @@ class AssembleCoaddConfig(CoaddBaseTask.ConfigClass):
     badMaskPlanes = pexConfig.ListField(
         dtype=str,
         doc="Mask planes that, if set, the associated pixel should not be included in coadd.",
-        default=("NO_DATA",),
+        default=("NO_DATA", "BAD", "EDGE", "SAT"),
     )
 
     def validate(self):
