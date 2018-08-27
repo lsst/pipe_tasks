78ad1b981aab009a24d27e52e912bfd38674a6ac
ktlim <ktl@slac.stanford.edu>
2018-08-27 00:25:37 +0000

    Add setup_module definition.

--- tests/test_psfCandidateSelection.py
+++ tests/test_psfCandidateSelection.py
@@ -62,6 +62,10 @@ class MemoryTester(lsst.utils.tests.MemoryTestCase):
     pass
 
 
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
 if __name__ == "__main__":
     lsst.utils.tests.init()
     unittest.main()
