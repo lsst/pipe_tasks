863affe01ceae654d91860dd50bb352412037a9a
danielsf <scott.f.daniel@gmail.com>
2019-10-07 15:57:29 -0700

    test_insertFakes.py creates and deletes dummy repo

--- tests/test_insertFakes.py
+++ tests/test_insertFakes.py
@@ -19,9 +19,47 @@
 # the GNU General Public License along with this program.  If not,
 # see <https://www.lsstcorp.org/LegalNotices/>.
 #
+import os
 import unittest
+import tempfile
+import shutil
 
 import lsst.utils.tests
+import lsst.utils as lsst_utils
+
+from lsst.pipe.tasks.processCcd import ProcessCcdTask
+
+
+class TestFakeInserstion(unittest.TestCase):
+    """
+    Test case for the methods in insertFakes.py
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        current_dir = os.path.join(lsst_utils.getPackageDir('pipe_tasks'),
+                                   'tests')
+        cls._workspace = tempfile.mkdtemp(dir=current_dir,
+                                          prefix='insertFakes_workspace_')
+
+        print('\nworkspace:\n%s\n\n' % cls._workspace)
+
+        obs_test_dir = lsst_utils.getPackageDir('obs_test')
+        input_dir = os.path.join(obs_test_dir, 'data', 'input')
+        cls._data_id = {'visit': 1}
+        data_id_string = ['%s=%s' % (key, val)
+                          for key, val in cls._data_id.items()]
+        ProcessCcdTask.parseAndRun(args=[input_dir, '--output', cls._workspace,
+                                         '--clobber-config', '--clobber-output',
+                                         '--doraise',
+                                         '--id'] + data_id_string)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls._workspace)
+
+    def test_dummy(self):
+        pass
 
 
 def setup_module(module):
