0df86b2c190ca282aca14c272622f3b30c782ede
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-12-10 04:46:06 +0000

    Add required setup_module() call.

--- tests/test_calexpCutout.py
+++ tests/test_calexpCutout.py
@@ -156,6 +156,10 @@ class MemoryTestCase(lsst.utils.tests.MemoryTestCase):
     pass
 
 
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
 if __name__ == "__main__":
     lsst.utils.tests.init()
     unittest.main()
