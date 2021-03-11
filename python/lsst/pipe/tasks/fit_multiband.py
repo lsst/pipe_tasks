8665a75d3612873f35299924b5d834643c1aea4f
taranu <dtaranu@astro.princeton.edu>
2021-03-10 18:39:48 -0600

    Add MultibandFitSubConfig to __all__

--- python/lsst/pipe/tasks/fit_multiband.py
+++ python/lsst/pipe/tasks/fit_multiband.py
@@ -19,7 +19,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ["CatalogExposure", "MultibandFitSubTask", "MultibandFitConfig", "MultibandFitTask"]
+__all__ = [
+    "CatalogExposure", "MultibandFitConfig", "MultibandFitSubConfig", "MultibandFitSubTask",
+    "MultibandFitTask",
+]
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
