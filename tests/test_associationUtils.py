eafea30e2df4a82144f9b3ca499767be5c0a7419
Christopher B. Morrison <morriscb@uw.edu>
2021-06-16 09:47:49 -0700

    Add setup_module.

--- tests/test_associationUtils.py
+++ tests/test_associationUtils.py
@@ -60,6 +60,10 @@ class MemoryTestCase(lsst.utils.tests.MemoryTestCase):
     pass
 
 
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
 if __name__ == "__main__":
     lsst.utils.tests.init()
     unittest.main()
