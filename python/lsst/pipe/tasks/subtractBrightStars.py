7ededfac16dc8eb09d99ddc34d948618ca356151
Morgan A Schmitz <morgan.schmitz@astro.princeton.edu>
2021-07-13 10:40:03 -0500

    Add bright star subtraction task (LSK edits)

--- python/lsst/pipe/tasks/subtractBrightStars.py
+++ python/lsst/pipe/tasks/subtractBrightStars.py
@@ -39,19 +39,19 @@ from lsst import geom
 
 class SubtractBrightStarsConnections(pipeBase.PipelineTaskConnections,
                                      dimensions=("instrument", "visit", "detector"),
-                                     defaultTemplates={"outputExposureName": "subtracted",
+                                     defaultTemplates={"outputExposureName": "brightStar_subtracted",
                                                        "outputBackgroundName": "brightStars"}):
     inputExposure = cT.Input(
-        doc="Input exposure from which to subtract bright star stamps",
+        doc="Input exposure from which to subtract bright star stamps.",
         name="calexp",
         storageClass="ExposureF",
-        dimensions=("visit", "detector")
+        dimensions=("visit", "detector",),
     )
     inputBrightStarStamps = cT.Input(
         doc="Set of preprocessed postage stamps, each centered on a single bright star.",
         name="brightStarStamps",
         storageClass="BrightStarStamps",
-        dimensions=("visit", "detector")
+        dimensions=("visit", "detector",),
     )
     inputExtendedPsf = cT.Input(
         doc="Extended PSF model.",
@@ -60,22 +60,22 @@ class SubtractBrightStarsConnections(pipeBase.PipelineTaskConnections,
         dimensions=("band",),
     )
     skyCorr = cT.Input(
-        doc="Input Sky Correction to be subtracted from the calexp if doApplySkyCorr=True",
+        doc="Input Sky Correction to be subtracted from the calexp if ``doApplySkyCorr``=True.",
         name="skyCorr",
         storageClass="Background",
-        dimensions=("instrument", "visit", "detector")
+        dimensions=("instrument", "visit", "detector",),
     )
     outputExposure = cT.Output(
         doc="Exposure with bright stars subtracted.",
         name="{outputExposureName}_calexp",
         storageClass="ExposureF",
-        dimensions=("visit", "detector"),
+        dimensions=("visit", "detector",),
     )
     outputBackgroundExposure = cT.Output(
         doc="Exposure containing only the modelled bright stars.",
         name="{outputBackgroundName}_calexp_background",
         storageClass="ExposureF",
-        dimensions=("visit", "detector"),
+        dimensions=("visit", "detector",),
     )
 
     def __init__(self, *, config=None):
@@ -112,12 +112,14 @@ class SubtractBrightStarsConfig(pipeBase.PipelineTaskConfig,
             "lanczos3": "Lanczos kernel of order 3",
             "lanczos4": "Lanczos kernel of order 4",
             "lanczos5": "Lanczos kernel of order 5",
+            "lanczos6": "Lanczos kernel of order 6",
+            "lanczos7": "Lanczos kernel of order 7",
         }
     )
     scalingType = pexConfig.ChoiceField(
         dtype=str,
         doc="How the model should be scaled to each bright star; implemented options are "
-            "annularFlux to reuse the annular flux of each stamp, or leastSquare to perform "
+            "`annularFlux` to reuse the annular flux of each stamp, or `leastSquare` to perform "
             "least square fitting on each pixel with no bad mask plane set.",
         default="leastSquare",
         allowed={
@@ -128,8 +130,11 @@ class SubtractBrightStarsConfig(pipeBase.PipelineTaskConfig,
     badMaskPlanes = pexConfig.ListField(
         dtype=str,
         doc="Mask planes that, if set, lead to associated pixels not being included in the computation of "
-            "the scaling factor. Ignored if scalingType is annularFlux, as the stamps are expected to "
-            "already be normalized.",
+            "the scaling factor (`BAD` should always be included). Ignored if scalingType is `annularFlux`, "
+            "as the stamps are expected to already be normalized.",
+            # Note that `BAD` should always be included, as secondary detected
+            # sources (i.e., detected sources other than the primary source of
+            # interest) also get set to `BAD`.
         default=('BAD', 'CR', 'CROSSTALK', 'EDGE', 'NO_DATA', 'SAT', 'SUSPECT', 'UNMASKEDNAN')
     )
     doApplySkyCorr = pexConfig.Field(
@@ -139,30 +144,30 @@ class SubtractBrightStarsConfig(pipeBase.PipelineTaskConfig,
     )
 
 
-class SubtractBrightStarsTask(pipeBase.CmdLineTask):
+class SubtractBrightStarsTask(pipeBase.PipelineTask, pipeBase.CmdLineTask):
     """Use an extended PSF model to subtract bright stars from a calibrated
     exposure (i.e. at single-visit level).
 
-    This task uses both an extended PSF model produced by
-    `lsst.pipe.tasks.extended_psf.MeasureExtendedPsfTask` and a set of bright
-    star stamps produced by
-    `lsst.pipe.tasks.processBrightStars.ProcessBrightStarsTask`.
+    This task uses both a set of bright star stamps produced by
+    `~lsst.pipe.tasks.processBrightStars.ProcessBrightStarsTask`
+    and an extended PSF model produced by
+    `~lsst.pipe.tasks.extended_psf.MeasureExtendedPsfTask`.
     """
     ConfigClass = SubtractBrightStarsConfig
     _DefaultName = "subtractBrightStars"
 
     def __init__(self, initInputs=None, *args, **kwargs):
-        pipeBase.CmdLineTask.__init__(self, *args, **kwargs)
-        # placeholders to set up Statistics if scalingType is leastSquare
+        super().__init__(self, *args, **kwargs)
+        # Placeholders to set up Statistics if scalingType is leastSquare.
         self.statsControl, self.statsFlag = None, None
 
     def _setUpStatistics(self, exampleMask):
-        """Configure statistics control and flag, for use if scalingType is
-        ``"leastSquare"``.
+        """Configure statistics control and flag, for use if ``scalingType`` is
+        `leastSquare`.
         """
-        if self.config.scalingType == 'leastSquare':
+        if self.config.scalingType == "leastSquare":
             self.statsControl = afwMath.StatisticsControl()
-            # set mask planes to be ignored
+            # Set the mask planes which will be ignored.
             andMask = reduce(ior, (exampleMask.getPlaneBitMask(bm) for bm in self.config.badMaskPlanes))
             self.statsControl.setAndMask(andMask)
             self.statsFlag = afwMath.stringToStatisticsProperty("SUM")
@@ -177,12 +182,11 @@ class SubtractBrightStarsTask(pipeBase.CmdLineTask):
 
         Parameters
         ----------
-        calexp : `lsst.afw.image.Exposure` or `lsst.afw.image.MaskedImage`
+        calexp : `~lsst.afw.image.Exposure` or `~lsst.afw.image.MaskedImage`
             Calibrated exposure.
-        skyCorr : `lsst.afw.math.backgroundList.BackgroundList` or None,
-                  optional
+        skyCorr : `~lsst.afw.math.backgroundList.BackgroundList`
             Full focal plane sky correction, obtained by running
-            `lsst.pipe.drivers.skyCorrection.SkyCorrectionTask`.
+            `~lsst.pipe.drivers.skyCorrection.SkyCorrectionTask`.
         """
         if isinstance(calexp, afwImage.Exposure):
             calexp = calexp.getMaskedImage()
@@ -194,10 +198,10 @@ class SubtractBrightStarsTask(pipeBase.CmdLineTask):
 
         Parameters
         ----------
-        model : `lsst.afw.image.MaskedImageF`
+        model : `~lsst.afw.image.MaskedImageF`
             The extended PSF model, shifted (and potentially warped) to match
             the bright star's positioning.
-        star : `lsst.meas.algorithms.brightStarStamps.BrightStarStamp`
+        star : `~lsst.meas.algorithms.brightStarStamps.BrightStarStamp`
             A stamp centered on the bright star to be subtracted.
         inPlace : `bool`
             Whether the model should be scaled in place. Default is `True`.
@@ -216,19 +220,20 @@ class SubtractBrightStarsTask(pipeBase.CmdLineTask):
             if self.statsControl is None:
                 self._setUpStatistics(star.stamp_im.mask)
             starIm = star.stamp_im.clone()
-            # rotate
+            # Rotate the star postage stamp.
             starIm = afwMath.rotateImageBy90(starIm, nb90Rots)
-            # "unnormalize" star
+            # Reverse the prior star flux normalization ("unnormalize").
             starIm *= star.annularFlux
-            # make sure both components use the star mask
-            XY = starIm.clone()
-            XY.image.array *= model.image.array
-            XX = starIm.clone()
-            XX.image.array = model.image.array**2
-            # compute scaling factor
-            XYSum = afwMath.makeStatistics(XY, self.statsFlag, self.statsControl).getValue()
-            XXSum = afwMath.makeStatistics(XX, self.statsFlag, self.statsControl).getValue()
-            scalingFactor = XYSum / XXSum if XXSum else 0
+            # The estimator of the scalingFactor (f) that minimizes (Y-fX)^2
+            # is E[XY]/E[XX].
+            xy = starIm.clone()
+            xy.image.array *= model.image.array
+            xx = starIm.clone()
+            xx.image.array = model.image.array**2
+            # Compute the least squares scaling factor.
+            xySum = afwMath.makeStatistics(xy, self.statsFlag, self.statsControl).getValue()
+            xxSum = afwMath.makeStatistics(xx, self.statsFlag, self.statsControl).getValue()
+            scalingFactor = xySum / xxSum if xxSum else 0
         else:
             raise AttributeError(f'Unknown scalingType "{self.config.scalingType}"; implemented options '
                                  'are "annularFlux" and "leastSquare".')
@@ -245,64 +250,68 @@ class SubtractBrightStarsTask(pipeBase.CmdLineTask):
         inputExposure : `afwImage.exposure.exposure.ExposureF`
             The image from which bright stars should be subtracted.
         inputBrightStarStamps :
-                `lsst.meas.algorithms.brightStarStamps.BrightStarStamps`
+                `~lsst.meas.algorithms.brightStarStamps.BrightStarStamps`
             Set of stamps centered on each bright star to be subtracted,
             produced by running
-            `lsst.pipe.tasks.processBrightStars.ProcessBrightStarsTask`.
-        inputExtendedPsf : `lsst.pipe.tasks.extended_psf.ExtendedPsf`
+            `~lsst.pipe.tasks.processBrightStars.ProcessBrightStarsTask`.
+        inputExtendedPsf : `~lsst.pipe.tasks.extended_psf.ExtendedPsf`
             Extended PSF model, produced by
-            `lsst.pipe.tasks.extended_psf.MeasureExtendedPsfTask`.
-        skyCorr : `lsst.afw.math.backgroundList.BackgroundList` or ``None``,
+            `~lsst.pipe.tasks.extended_psf.MeasureExtendedPsfTask`.
+        skyCorr : `~lsst.afw.math.backgroundList.BackgroundList` or `None`,
                   optional
             Full focal plane sky correction, obtained by running
-            `lsst.pipe.drivers.skyCorrection.SkyCorrectionTask`.
-        dataId : `dict` or `lsst.daf.butler.DataCoordinate`
+            `~lsst.pipe.drivers.skyCorrection.SkyCorrectionTask`. If
+            `doApplySkyCorr` is set to `True`, `skyCorr` cannot be `None`.
+        dataId : `dict` or `~lsst.daf.butler.DataCoordinate`
             The dataId of the exposure (and detector) bright stars should be
             subtracted from.
 
         Returns
         -------
         subtractorExp : `afwImage.ExposureF`
-            An Exposure containing every bright star's profile; its image can
-            then be subtracted from the input exposure.
+            An Exposure containing a scaled bright star model fit to every
+            bright star profile; its image can then be subtracted from the
+            input exposure.
         invImages : `list` [`afwImage.MaskedImageF`]
             A list of small images ("stamps") containing the model, each scaled
             to its corresponding input bright star.
         """
         inputExpBBox = inputExposure.getBBox()
-        if self.config.doApplySkyCorr:
+        if self.config.doApplySkyCorr and (skyCorr is not None):
             self.log.info("Applying sky correction to exposure %s (exposure will be modified in-place).",
                           dataId)
             self.applySkyCorr(inputExposure, skyCorr)
-        # Create an empty image the size of the exposure
-        # TODO: DM-31085 set mask planes
+        # Create an empty image the size of the exposure.
+        # TODO: DM-31085 (set mask planes).
         subtractorExp = afwImage.ExposureF(bbox=inputExposure.getBBox())
         subtractor = subtractorExp.maskedImage
-        # make a copy of the input model
+        # Make a copy of the input model.
         model = inputExtendedPsf(dataId["detector"]).clone()
         modelStampSize = model.getDimensions()
         inv90Rots = 4 - inputBrightStarStamps.nb90Rots
         model = afwMath.rotateImageBy90(model, inv90Rots)
         warpCont = afwMath.WarpingControl(self.config.warpingKernelName)
         invImages = []
+        # Loop over bright stars, computing the inverse transformed and scaled
+        # postage stamp for each.
         for star in inputBrightStarStamps:
             if star.gaiaGMag < self.config.magLimit:
-                # set origin
+                # Set the origin.
                 model.setXY0(star.position)
-                # create empty destination image
+                # Create an empty destination image.
                 invTransform = star.archive_element.inverted()
                 invOrigin = geom.Point2I(invTransform.applyForward(geom.Point2D(star.position)))
                 bbox = geom.Box2I(corner=invOrigin, dimensions=modelStampSize)
                 invImage = afwImage.MaskedImageF(bbox)
-                # Apply inverse transform
+                # Apply inverse transform.
                 goodPix = afwMath.warpImage(invImage, model, invTransform, warpCont)
                 if not goodPix:
                     self.log.debug(f"Warping of a model failed for star {star.gaiaId}: "
                                    "no good pixel in output")
-                # Scale model
+                # Scale the model.
                 self.scaleModel(invImage, star, inPlace=True, nb90Rots=inv90Rots)
-                # Replace nans before subtraction (note all nan pixels have
-                # the NO_DATA flag)
+                # Replace NaNs before subtraction (note all NaN pixels have
+                # the NO_DATA flag).
                 invImage.image.array[np.isnan(invImage.image.array)] = 0
                 bbox.clip(inputExpBBox)
                 if bbox.getArea() > 0:
