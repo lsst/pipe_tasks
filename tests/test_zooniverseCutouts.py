eff79dc1c3afd5258b6680006a132e91ab0cf23c
John Parejko <parejkoj@uw.edu>
2021-07-23 17:27:54 -0700

    Add ZooniverseCutouts and test
    Add task topic page for ZooniverseCutouts.
    
    ZooniverseCutouts is not complete here (need apdb `where` support), but we can
    start using it now for image validation while we sort that out.

---
+++ tests/test_zooniverseCutouts.py
@@ -0,0 +1,129 @@
+# This file is part of pipe_tasks.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import os
+import pandas as pd
+import PIL
+import unittest
+import tempfile
+
+import lsst.afw.table
+import lsst.geom
+import lsst.meas.base.tests
+from lsst.pipe.tasks.zooniverseCutouts import ZooniverseCutoutsTask
+import lsst.utils.tests
+
+
+class TestZooniverseCutouts(lsst.utils.tests.TestCase):
+    """Test that ZooniverseCutoutsTask generates images and manifest files
+    correctly.
+    """
+    def setUp(self):
+        bbox = lsst.geom.Box2I(lsst.geom.Point2I(0, 0), lsst.geom.Point2I(100, 100))
+        self.centroid = lsst.geom.Point2D(65, 70)
+        dataset = lsst.meas.base.tests.TestDataset(bbox)
+        dataset.addSource(instFlux=1e5, centroid=self.centroid)
+        self.science, self.scienceCat = dataset.realize(noise=1000.0, schema=dataset.makeMinimalSchema())
+        lsst.afw.table.updateSourceCoords(self.science.wcs, self.scienceCat)
+        self.skyCenter = self.scienceCat[0].getCoord()
+        self.template, self.templateCat = dataset.realize(noise=5.0, schema=dataset.makeMinimalSchema())
+        # A simple and incorrect image difference to have something to plot.
+        self.difference = lsst.afw.image.ExposureF(self.science, deep=True)
+        self.difference.image.array -= self.template.image.array
+
+    def test_generate_image(self):
+        """Test that we get some kind of image out.
+
+        It's useful to have a person look at the output via:
+            im.show()
+        """
+        zooniverseCutouts = ZooniverseCutoutsTask()
+        cutout = zooniverseCutouts.generate_image(self.science, self.template, self.difference,
+                                                  self.skyCenter)
+        im = PIL.Image.open(cutout)
+        # NOTE: uncomment this to show the resulting image.
+        # im.show()
+        self.assertEqual(im.height, 233)
+        self.assertEqual(im.width, 630)
+
+        # A different cutout size: the resulting cutout image is the same
+        # size but shows more pixels.
+        config = ZooniverseCutoutsTask.ConfigClass()
+        config.size = 100
+        zooniverseCutouts = ZooniverseCutoutsTask(config=config)
+        cutout = zooniverseCutouts.generate_image(self.science, self.template, self.difference,
+                                                  self.skyCenter)
+        im = PIL.Image.open(cutout)
+        # NOTE: uncomment this to show the resulting image.
+        # im.show()
+        self.assertEqual(im.height, 233)
+        self.assertEqual(im.width, 630)
+
+    def test_generate_images(self):
+        """Test that images get written to a temporary directory."""
+        data = pd.DataFrame(data={"diaSourceId": [5, 10],
+                                  "ra": [45.001, 45.002],
+                                  "decl": [45.0, 45.001],
+                                  "ccd": [50, 60],
+                                  "visit": [1234, 5678]})
+        butler = unittest.mock.Mock(spec=lsst.daf.butler.Butler)
+        # we don't care what the output images look like here, just that
+        # butler.get() returns an Exposure for every call.
+        butler.get.return_value = self.science
+
+        with tempfile.TemporaryDirectory() as path:
+            config = ZooniverseCutoutsTask.ConfigClass()
+            config.outputPath = path
+            zooniverseCutouts = ZooniverseCutoutsTask(config)
+            zooniverseCutouts.generate_images(data, butler)
+            image = PIL.Image.open(os.path.join(path, "images/5.png"))
+            self.assertEqual(image.format, "PNG")
+            PIL.Image.open(os.path.join(path, "images/10.png"))
+            self.assertEqual(image.format, "PNG")
+
+    def check_make_manifest(self, url_root):
+        """Check that make_manifest returns an appropriate DataFrame.
+        """
+        data = pd.DataFrame(data={"diaSourceId": [5, 10, 20], "otherField": [3, 2, 1]})
+        config = ZooniverseCutoutsTask.ConfigClass()
+        config.urlRoot = url_root
+        zooniverseCutouts = ZooniverseCutoutsTask(config=config)
+        manifest = zooniverseCutouts.make_manifest(data)
+        self.assertEqual(manifest['metadata:diaSourceId'].to_list(),
+                         [5, 10, 20])
+        return manifest
+
+    def test_make_manifest(self):
+        # check with an ending slash
+        root = "http://example.org/zooniverse"
+        manifest = self.check_make_manifest(root)
+        self.assertEqual(manifest['location:1'].to_list(),
+                         [f"{root}/5.png",
+                          f"{root}/10.png",
+                          f"{root}/20.png"])
+
+        # check without an ending slash
+        root = "http://example.org/zooniverse/"
+        manifest = self.check_make_manifest(root)
+        self.assertEqual(manifest['location:1'].to_list(),
+                         [f"{root}5.png",
+                          f"{root}10.png",
+                          f"{root}20.png"])
