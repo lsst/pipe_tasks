16680e5997fc1bab31dbf480cd60f3af1d5b0e93
John Parejko <parejkoj@uw.edu>
2022-04-08 16:39:57 -0700

    Add gen2 compatibility refcat lines

--- python/lsst/pipe/tasks/calibrate.py
+++ python/lsst/pipe/tasks/calibrate.py
@@ -325,6 +325,10 @@ class CalibrateConfig(pipeBase.PipelineTaskConfig, pipelineConnections=Calibrate
         # The photoRefCat connection is the name to use for the colorterms.
         self.photoCal.photoCatName = self.connections.photoRefCat
 
+        # NOTE: these two lines are for gen2, and are only here for compatibility.
+        self.astromRefObjLoader.ref_dataset_name = "gaia_dr2_20200414"
+        self.photoRefObjLoader.ref_dataset_name = "ps1_pv3_3pi_20170110"
+
     def validate(self):
         super().validate()
         astromRefCatGen2 = getattr(self.astromRefObjLoader, "ref_dataset_name", None)
