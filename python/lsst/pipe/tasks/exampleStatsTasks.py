0d3dd193afa9e49a897c5089857d2031ef25c67d
Nate Lust <nlust@astro.princeton.edu>
2022-06-29 10:59:43 -0400

    Change documentation in exampleStatsTasks
    Change made as a part of 2022 bootcamp.
    
    Co-authored-by: Neven Caplar <ncaplar@princeton.edu>

--- python/lsst/pipe/tasks/exampleStatsTasks.py
+++ python/lsst/pipe/tasks/exampleStatsTasks.py
@@ -27,13 +27,15 @@ from lsst.utils.timer import timeMethod
 
 __all__ = ["ExampleSigmaClippedStatsConfig", "ExampleSigmaClippedStatsTask", "ExampleSimpleStatsTask"]
 
-# The following block adds links to these tasks from the Task Documentation page.
+# The following block adds links to these tasks from the Task Documentation
+#  page.
 # This works even for task(s) that are not in lsst.pipe.tasks.
 ## \addtogroup LSST_task_documentation
 ## \{
 ## \page page_exampleStatsTasks ExampleStatsTasks
 ## \ref ExampleSigmaClippedStatsTask "ExampleSigmaClippedStatsTask"
-##      A simple example subtask that computes sigma-clipped statistics of an image
+##      A simple example subtask that computes sigma-clipped statistics of an
+##      image
 ## <br>
 ## \ref ExampleSimpleStatsTask "ExampleSimpleStatsTask"
 ##      A very simple example subtask that computes statistics of an image.
@@ -62,52 +64,38 @@ class ExampleSigmaClippedStatsConfig(pexConfig.Config):
 
 
 class ExampleSigmaClippedStatsTask(pipeBase.Task):
-    r"""!
-    Example task to compute sigma-clipped mean and standard deviation of an image
+    r"""Example task to compute sigma-clipped mean and standard deviation of an
+    image
 
-    \section pipeTasks_ExampleSigmaClippedStatsTask_Contents Contents
+    This is a simple example task designed to be run as a subtask by
+    ExampleCmdLineTask. See also ExampleSimpleStatsTask as a variant that is
+    even simpler.
 
-     - \ref pipeTasks_ExampleSigmaClippedStatsTask_Purpose
-     - \ref pipeTasks_ExampleSigmaClippedStatsTask_Config
-     - \ref pipeTasks_ExampleSigmaClippedStatsTask_Debug
-     - \ref pipeTasks_ExampleSigmaClippedStatsTask_Example
+    The main method is ExampleSigmaClippedStatsTask.run "run".
 
-    \section pipeTasks_ExampleSigmaClippedStatsTask_Purpose Description
+    pipeTasks_ExampleSigmaClippedStatsTask_Config  Configuration parameters
 
-    \copybrief ExampleSigmaClippedStatsTask
+    See ExampleSigmaClippedStatsConfig
 
-    This is a simple example task designed to be run as a subtask.
-    See also ExampleSimpleStatsTask as a variant that is even simpler.
-
-    The main method is \ref ExampleSigmaClippedStatsTask.run "run".
-
-    \section pipeTasks_ExampleSigmaClippedStatsTask_Config  Configuration parameters
-
-    See \ref ExampleSigmaClippedStatsConfig
-
-    \section pipeTasks_ExampleSigmaClippedStatsTask_Debug   Debug variables
+    pipeTasks_ExampleSigmaClippedStatsTask_Debug   Debug variables
 
     This task has no debug variables.
 
-    \section pipeTasks_ExampleSigmaClippedStatsTask_Example A complete example
+    pipeTasks_ExampleSigmaClippedStatsTask_Example A complete example
     of using ExampleSigmaClippedStatsTask
 
     This code is in examples/exampleStatsTask.py (this one example runs both
-    ExampleSigmaClippedStatsTask and ExampleSimpleStatsTask), and can be run as:
-    \code
-    examples/exampleStatsTask.py [fitsFile]
-    \endcode
+    ExampleSigmaClippedStatsTask and ExampleSimpleStatsTask), and can be run
+    as: examples/exampleStatsTask.py [fitsFile]
+
+    The init method may compute anything that that does not require data.
+    In this case we create a statistics control object using the config
+    (which cannot change once the task is created).
     """
     ConfigClass = ExampleSigmaClippedStatsConfig
     _DefaultName = "exampleSigmaClippedStats"
 
     def __init__(self, *args, **kwargs):
-        """!Construct an ExampleSigmaClippedStatsTask
-
-        The init method may compute anything that that does not require data.
-        In this case we create a statistics control object using the config
-        (which cannot change once the task is created).
-        """
         pipeBase.Task.__init__(self, *args, **kwargs)
 
         self._badPixelMask = afwImage.Mask.getPlaneBitMask(self.config.badMaskPlanes)
@@ -119,14 +107,21 @@ class ExampleSigmaClippedStatsTask(pipeBase.Task):
 
     @timeMethod
     def run(self, maskedImage):
-        """!Compute and return statistics for a masked image
+        """Compute and return statistics for a masked image
 
-        @param[in] maskedImage: masked image (an lsst::afw::MaskedImage)
-        @return a pipeBase Struct containing:
-        - mean: mean of image plane
-        - meanErr: uncertainty in mean
-        - stdDev: standard deviation of image plane
-        - stdDevErr: uncertainty in standard deviation
+        Parameters
+        ----------
+        maskedImage : `lsst.afw.MaskedImage`
+            masked image
+
+        Returns
+        -------
+        retStruct : `~lsst.pipe.base.Struct`
+            A struct containing following attributes
+            - mean: mean of image plane
+            - meanErr: uncertainty in mean
+            - stdDev: standard deviation of image plane
+            - stdDevErr: uncertainty in standard deviation
         """
         statObj = afwMath.makeStatistics(maskedImage, afwMath.MEANCLIP | afwMath.STDEVCLIP | afwMath.ERRORS,
                                          self._statsControl)
@@ -143,62 +138,63 @@ class ExampleSigmaClippedStatsTask(pipeBase.Task):
 
 
 class ExampleSimpleStatsTask(pipeBase.Task):
-    r"""!
+    r"""
     Example task to compute mean and standard deviation of an image
 
-    \section pipeTasks_ExampleSimpleStatsTask_Contents Contents
+    This was designed to be run as a subtask by ExampleCmdLineTask.
+    It is about as simple as a task can be; it has no configuration parameters
+    and requires no special initialization. See also
+    ExampleSigmaClippedStatsTask as a variant that is slightly more
+    complicated.
 
-     - \ref pipeTasks_ExampleSimpleStatsTask_Purpose
-     - \ref pipeTasks_ExampleSimpleStatsTask_Config
-     - \ref pipeTasks_ExampleSimpleStatsTask_Debug
-     - \ref pipeTasks_ExampleSimpleStatsTask_Example
+    The main method is ExampleSimpleTask.run "run".
 
-    \section pipeTasks_ExampleSimpleStatsTask_Purpose Description
-
-    \copybrief ExampleSimpleStatsTask
-
-    This was designed to be run as a subtask.
-    It is about as simple as a task can be; it has no configuration parameters and requires no special
-    initialization. See also ExampleSigmaClippedStatsTask as a variant that is slightly more complicated.
-
-    The main method is \ref ExampleSimpleTask.run "run".
-
-    \section pipeTasks_ExampleSimpleStatsTask_Config    Configuration parameters
+    pipeTasks_ExampleSimpleStatsTask_Config    Configuration parameters
 
     This task has no configuration parameters.
 
-    \section pipeTasks_ExampleSimpleStatsTask_Debug     Debug variables
+    pipeTasks_ExampleSimpleStatsTask_Debug     Debug variables
 
     This task has no debug variables.
 
-    \section pipeTasks_ExampleSimpleStatsTask_Example A complete example of using ExampleSimpleStatsTask
+    pipeTasks_ExampleSimpleStatsTask_Example A complete example of using
+    ExampleSimpleStatsTask
 
     This code is in examples/exampleStatsTask.py (this one example runs both
-    ExampleSigmaClippedStatsTask and ExampleSimpleStatsTask), and can be run as:
-    \code
-    examples/exampleStatsTask.py [fitsFile]
-    \endcode
+    ExampleSigmaClippedStatsTask and ExampleSimpleStatsTask), and can be run
+    as: examples/exampleStatsTask.py [fitsFile]
     """
     # Even a task with no configuration requires setting ConfigClass
     ConfigClass = pexConfig.Config
-    # Having a default name simplifies construction of the task, since the parent task
-    # need not specify a name. Note: having a default name is required for pipeline tasks.
-    # The name can be simple and need not be unique (except for multiple subtasks that will
-    # be run by a parent task at the same time).
+    # Having a default name simplifies construction of the task, since the
+    # parent task need not specify a name. Note: having a default name is
+    # required for command-line tasks.
+    # The name can be simple and need not be unique (except for multiple
+    # subtasks that will be run by a parent task at the same time).
     _DefaultName = "exampleSimpleStats"
 
-    # The `lsst.utils.timer.timeMethod` decorator measures how long a task method takes to run,
-    # and the resources needed to run it. The information is recorded in the task's `metadata` field.
+    # The `lsst.utils.timer.timeMethod` decorator measures how long a task
+    # method takes to run, and the resources needed to run it. The information
+    # is recorded in the task's `metadata` field.
+    # Most command-line tasks (not including the example below) save metadata
+    # for the task and all of its subtasks whenver the task is run.
     @timeMethod
     def run(self, maskedImage):
-        """!Compute and return statistics for a masked image
+        """Compute and return statistics for a masked image
 
-        @param[in] maskedImage: masked image (an lsst::afw::MaskedImage)
-        @return a pipeBase Struct containing:
-        - mean: mean of image plane
-        - meanErr: uncertainty in mean
-        - stdDev: standard deviation of image plane
-        - stdDevErr: uncertainty in standard deviation
+        Parameters
+        ----------
+        maskedImage : `lsst.afw.MaskedImage`
+            masked image
+
+        Returns
+        -------
+        retStruct : `~lsst.pipe.base.Struct`
+            A struct containing following attributes
+            - mean: mean of image plane
+            - meanErr: uncertainty in mean
+            - stdDev: standard deviation of image plane
+            - stdDevErr: uncertainty in standard deviation
         """
         self._statsControl = afwMath.StatisticsControl()
         statObj = afwMath.makeStatistics(maskedImage, afwMath.MEAN | afwMath.STDEV | afwMath.ERRORS,
