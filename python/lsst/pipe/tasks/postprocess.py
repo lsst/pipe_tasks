0827c807a9f4978c929bbebbf678ed49e7bc17f6
Lauren MacArthur <lauren@astro.princeton.edu>
2022-12-30 11:51:14 -0800

    Add PSF model robustness metrics to ccdVisitTable

--- python/lsst/pipe/tasks/postprocess.py
+++ python/lsst/pipe/tasks/postprocess.py
@@ -1436,7 +1436,8 @@ class MakeCcdVisitTableTask(pipeBase.PipelineTask):
                              'psfStarDeltaE1Median', 'psfStarDeltaE2Median',
                              'psfStarDeltaE1Scatter', 'psfStarDeltaE2Scatter',
                              'psfStarDeltaSizeMedian', 'psfStarDeltaSizeScatter',
-                             'psfStarScaledDeltaSizeScatter']
+                             'psfStarScaledDeltaSizeScatter',
+                             'psfTraceRadiusDelta', 'maxDistToNearestPsf']
             ccdEntry = summaryTable[selectColumns].to_pandas().set_index('id')
             # 'visit' is the human readable visit number.
             # 'visitId' is the key to the visitId table. They are the same.
