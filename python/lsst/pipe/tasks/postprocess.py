f2fe09332996a13c0f0d91d8a41e3ba970af705c
Jim Bosch <jbosch@astro.princeton.edu>
2023-01-04 15:12:22 -0500

    Add initOutput schema to ConsolidateVisitSummary.

--- python/lsst/pipe/tasks/postprocess.py
+++ python/lsst/pipe/tasks/postprocess.py
@@ -1226,6 +1226,11 @@ class ConsolidateVisitSummaryConnections(pipeBase.PipelineTaskConnections,
         storageClass="ExposureCatalog",
         dimensions=("instrument", "visit"),
     )
+    visitSummarySchema = connectionTypes.InitOutput(
+        doc="Schema of the visitSummary catalog",
+        name="{calexpType}visitSummary_schema",
+        storageClass="ExposureCatalog",
+    )
 
 
 class ConsolidateVisitSummaryConfig(pipeBase.PipelineTaskConfig,
@@ -1255,6 +1260,15 @@ class ConsolidateVisitSummaryTask(pipeBase.PipelineTask):
     _DefaultName = "consolidateVisitSummary"
     ConfigClass = ConsolidateVisitSummaryConfig
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.schema = afwTable.ExposureTable.makeMinimalSchema()
+        self.schema.addField('visit', type='L', doc='Visit number')
+        self.schema.addField('physical_filter', type='String', size=32, doc='Physical filter')
+        self.schema.addField('band', type='String', size=32, doc='Name of band')
+        ExposureSummaryStats.update_schema(self.schema)
+        self.visitSummarySchema = afwTable.ExposureCatalog(self.schema)
+
     def runQuantum(self, butlerQC, inputRefs, outputRefs):
         dataRefs = butlerQC.get(inputRefs.calexp)
         visit = dataRefs[0].dataId.byName()['visit']
@@ -1283,8 +1297,7 @@ class ConsolidateVisitSummaryTask(pipeBase.PipelineTask):
         visitSummary : `lsst.afw.table.ExposureCatalog`
             Exposure catalog with per-detector summary information.
         """
-        schema = self._makeVisitSummarySchema()
-        cat = afwTable.ExposureCatalog(schema)
+        cat = afwTable.ExposureCatalog(self.schema)
         cat.resize(len(dataRefs))
 
         cat['visit'] = visit
@@ -1320,15 +1333,6 @@ class ConsolidateVisitSummaryTask(pipeBase.PipelineTask):
 
         cat.sort()
         return cat
-
-    def _makeVisitSummarySchema(self):
-        """Make the schema for the visitSummary catalog."""
-        schema = afwTable.ExposureTable.makeMinimalSchema()
-        schema.addField('visit', type='L', doc='Visit number')
-        schema.addField('physical_filter', type='String', size=32, doc='Physical filter')
-        schema.addField('band', type='String', size=32, doc='Name of band')
-        ExposureSummaryStats.update_schema(schema)
-        return schema
 
 
 class ConsolidateSourceTableConnections(pipeBase.PipelineTaskConnections,
