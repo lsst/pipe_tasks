Fix bad Ebv units with pandas 2 and add associated tests.

tests/test_functors.py
@@ -44,7 +44,7 @@ from lsst.pipe.tasks.functors import (CompositeFunctor, CustomFunctor, Column, R
                                       LocalDipoleMeanFlux, LocalDipoleMeanFluxErr,
                                       LocalDipoleDiffFlux, LocalDipoleDiffFluxErr,
                                       LocalWcs, ComputePixelScale, ConvertPixelToArcseconds,
-                                      ConvertPixelSqToArcsecondsSq, Ratio, HtmIndex20)
+                                      ConvertPixelSqToArcsecondsSq, Ratio, HtmIndex20, Ebv)
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -751,6 +751,18 @@ class FunctorTestCase(unittest.TestCase):
             val.values,
             [14924528684992, 14924528689697, 14924528501716, 14924526434259,
              14924526433879])))
+
+    def testEbv(self):
+        """Test that EBV works.
+        """
+        df = self.getMultiIndexDataFrame(self.dataDict)
+        func = Ebv()
+
+        val = self._funcVal(func, df)
+        np.testing.assert_array_almost_equal(
+            val.values,
+            [0.029100, 0.029013, 0.028857, 0.028802, 0.028797]
+        )
 
     def _dropLevels(self, df):
         levelsToDrop = [n for lev, n in zip(df.columns.levels, df.columns.names) if len(lev) == 1]