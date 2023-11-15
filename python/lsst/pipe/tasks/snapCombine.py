f99b359ef9010abb2d44fc88c1c9dd2d0a28cf7c
John Parejko <parejkoj@uw.edu>
2023-11-15 13:53:34 -0800

    Remove unnecessary components
    Until we know what we want SnapCombine to actually do, remove almost
    everything that was previously implemented so that the config namespace
    is clean. We can always pull it out of git history if we want it.
    
    * Modernize method/variable names.

--- python/lsst/pipe/tasks/snapCombine.py
+++ python/lsst/pipe/tasks/snapCombine.py
@@ -19,129 +19,26 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-__all__ = ["InitialPsfConfig", "SnapCombineConfig", "SnapCombineTask"]
+__all__ = ["SnapCombineConfig", "SnapCombineTask"]
 
-import numpy as num
 import lsst.pex.config as pexConfig
 import lsst.daf.base as dafBase
 import lsst.afw.image as afwImage
-import lsst.afw.table as afwTable
 import lsst.pipe.base as pipeBase
-from lsstDebug import getDebugFrame
-from lsst.afw.display import getDisplay
 from lsst.coadd.utils import addToCoadd, setCoaddEdgeBits
-from lsst.meas.algorithms import SourceDetectionTask
-from lsst.meas.base import SingleFrameMeasurementTask
-import lsst.meas.algorithms as measAlg
 from lsst.utils.timer import timeMethod
-
-from .repair import RepairTask
-
-
-class InitialPsfConfig(pexConfig.Config):
-    """Describes the initial PSF used for detection and measurement before we do PSF determination."""
-
-    model = pexConfig.ChoiceField(
-        dtype=str,
-        doc="PSF model to install in snaps.",
-        default="SingleGaussian",
-        allowed={
-            "SingleGaussian": "Single Gaussian model",
-            "DoubleGaussian": "Double Gaussian model",
-        },
-    )
-    fwhm = pexConfig.Field(
-        dtype=float,
-        doc="FWHM of PSF model (arcsec)",
-        default=1.0,
-    )
-    size = pexConfig.Field(
-        dtype=int,
-        doc="Size of PSF model (pixels)",
-        default=15,
-    )
 
 
 class SnapCombineConfig(pexConfig.Config):
-    doRepair = pexConfig.Field(
-        dtype=bool,
-        doc="Repair images (CR reject and interpolate) before combining",
-        default=False,
-    )
-    repairPsfFwhm = pexConfig.Field(
-        dtype=float,
-        doc="Psf FWHM (pixels) used to detect CRs",
-        default=2.5,
-    )
-    doDiffIm = pexConfig.Field(
-        dtype=bool,
-        doc="Perform difference imaging before combining",
-        default=False,
-    )
-    doMeasurement = pexConfig.Field(
-        dtype=bool,
-        doc="Measure difference sources (ignored if doDiffIm false)",
-        default=True,
-    )
     badMaskPlanes = pexConfig.ListField(
         dtype=str,
         doc="Mask planes that, if set, the associated pixels are not included in the combined exposure.",
         default=(),
     )
-    averageKeys = pexConfig.ListField(
-        dtype=str,
-        doc="List of float metadata keys to average when combining snaps, e.g. float positions and dates; "
-        "non-float data must be handled by overriding the fixMetadata method",
-        optional=True,
-    )
-    sumKeys = pexConfig.ListField(
-        dtype=str,
-        doc="List of float or int metadata keys to sum when combining snaps, e.g. exposure time; "
-        "non-float, non-int data must be handled by overriding the fixMetadata method",
-        optional=True,
-    )
-    initialPsf = pexConfig.ConfigField(
-        dtype=InitialPsfConfig,
-        doc="Task to generate an initial PSF before repair."
-    )
-    repair = pexConfig.ConfigurableField(
-        target=RepairTask,
-        doc="Task to repair individual snaps before combining."
-    )
-    detection = pexConfig.ConfigurableField(
-        target=SourceDetectionTask,
-        doc="Task to perform detection on a difference image, if doDiffIm is True."
-    )
-    measurement = pexConfig.ConfigurableField(
-        target=SingleFrameMeasurementTask,
-        doc="Task to perform measurement on difference image, if doDiffIm is True."
-    )
-
-    def setDefaults(self):
-        self.detection.thresholdPolarity = "both"
-
-    def validate(self):
-        if self.detection.thresholdPolarity != "both":
-            raise ValueError("detection.thresholdPolarity must be 'both' for SnapCombineTask")
 
 
 class SnapCombineTask(pipeBase.Task):
     """Combine two snaps into a single visit image.
-
-    Notes
-    -----
-    Debugging:
-    The `~lsst.base.lsstDebug` variables in SnapCombineTask are:
-
-    display
-    A dictionary containing debug point names as keys with frame number as value. Valid keys are:
-
-    .. code-block:: none
-
-        repair0
-        Display the first snap after repairing.
-        repair1
-        Display the second snap after repairing.
     """
 
     ConfigClass = SnapCombineConfig
@@ -149,18 +46,10 @@ class SnapCombineTask(pipeBase.Task):
 
     def __init__(self, *args, **kwargs):
         pipeBase.Task.__init__(self, *args, **kwargs)
-        self.makeSubtask("repair")
-        self.schema = afwTable.SourceTable.makeMinimalSchema()
-        self.algMetadata = dafBase.PropertyList()
-        if self.config.doDiffIm:
-            self.makeSubtask("detection", schema=self.schema)
-            if self.config.doMeasurement:
-                self.makeSubtask("measurement", schema=self.schema, algMetadata=self.algMetadata)
 
     @timeMethod
-    def run(self, snap0, snap1, defects=None):
-        """Combine two snaps, returning the combined image and possibly doing
-        simple image subtraction on the two.
+    def run(self, snap0, snap1):
+        """Combine two snaps, returning the combined image.
 
         Parameters
         ----------
@@ -168,8 +57,6 @@ class SnapCombineTask(pipeBase.Task):
             Snapshot exposure 0.
         snap1 : `lsst.afw.image.Exposure`
             Snapshot exposure 1.
-        defects : `list` or `None`, optional
-            Defect list (for repair task).
 
         Returns
         -------
@@ -178,59 +65,14 @@ class SnapCombineTask(pipeBase.Task):
 
             ``exposure``
                 Snap-combined exposure.
-            ``sources``
-                Detected sources, or `None` if detection not performed.
         """
-        # initialize optional outputs
-        sources = None
-
-        if self.config.doRepair:
-            psf = self.makeInitialPsf(snap0, fwhmPix=self.config.repairPsfFwhm)
-            snap0.setPsf(psf)
-            snap1.setPsf(psf)
-            self.log.info("Repairing snap image 1")
-            self.repair.run(snap0, defects=defects, keepCRs=False)
-            self.log.info("Repairing snap image 2")
-            self.repair.run(snap1, defects=defects, keepCRs=False)
-
-            repair0frame = getDebugFrame(self._display, "repair0")
-            if repair0frame:
-                getDisplay(repair0frame).mtv(snap0)
-            repair1frame = getDebugFrame(self._display, "repair1")
-            if repair1frame:
-                getDisplay(repair1frame).mtv(snap1)
-
-        if self.config.doDiffIm:
-            diffExp = afwImage.ExposureF(snap0, True)
-            diffMi = diffExp.maskedImage
-            diffMi -= snap1.maskedImage
-
-            psf = self.makeInitialPsf(snap0)
-            diffExp.setPsf(psf)
-            table = afwTable.SourceTable.make(self.schema)
-            table.setMetadata(self.algMetadata)
-            detRet = self.detection.run(table, diffExp)
-            sources = detRet.sources
-            if self.config.doMeasurement:
-                self.measurement.measure(diffExp, sources)
-
-            mask0 = snap0.mask
-            mask1 = snap1.mask
-            detRet.positive.setMask(mask0, "DETECTED")
-            detRet.negative.setMask(mask1, "DETECTED")
-
-            maskD = diffExp.mask
-            detRet.positive.setMask(maskD, "DETECTED")
-            detRet.negative.setMask(maskD, "DETECTED_NEGATIVE")
-
-        combinedExp = self.addSnaps(snap0, snap1)
+        combined = self._add_snaps(snap0, snap1)
 
         return pipeBase.Struct(
-            exposure=combinedExp,
-            sources=sources,
+            exposure=combined,
         )
 
-    def addSnaps(self, snap0, snap1):
+    def _add_snaps(self, snap0, snap1):
         """Add two snap exposures together, returning a new exposure.
 
         Parameters
@@ -264,89 +106,3 @@ class SnapCombineTask(pipeBase.Task):
         # note: none of the inputs has a valid PhotoCalib object, so that is not touched
         # Filter was already copied
 
-        combinedMetadata = combinedExp.getMetadata()
-        metadata0 = snap0.getMetadata()
-        metadata1 = snap1.getMetadata()
-        self.fixMetadata(combinedMetadata, metadata0, metadata1)
-
-        return combinedExp
-
-    def fixMetadata(self, combinedMetadata, metadata0, metadata1):
-        """Fix the metadata of the combined exposure (in place).
-
-        This implementation handles items specified by config.averageKeys and config.sumKeys,
-        which have data type restrictions. To handle other data types (such as sexagesimal
-        positions and ISO dates) you must supplement this method with your own code.
-
-        Parameters
-        ----------
-        combinedMetadata : `lsst.daf.base.PropertySet`
-            Metadata of combined exposure;
-            on input this is a deep copy of metadata0 (a PropertySet).
-        metadata0 : `lsst.daf.base.PropertySet`
-            Metadata of snap0 (a PropertySet).
-        metadata1 : `lsst.daf.base.PropertySet`
-            Metadata of snap1 (a PropertySet).
-
-        Notes
-        -----
-        The inputs are presently PropertySets due to ticket #2542. However, in some sense
-        they are just PropertyLists that are missing some methods. In particular: comments and order
-        are preserved if you alter an existing value with set(key, value).
-        """
-        keyDoAvgList = []
-        if self.config.averageKeys:
-            keyDoAvgList += [(key, 1) for key in self.config.averageKeys]
-        if self.config.sumKeys:
-            keyDoAvgList += [(key, 0) for key in self.config.sumKeys]
-        for key, doAvg in keyDoAvgList:
-            opStr = "average" if doAvg else "sum"
-            try:
-                val0 = metadata0.getScalar(key)
-                val1 = metadata1.getScalar(key)
-            except Exception:
-                self.log.warning("Could not %s metadata %r: missing from one or both exposures", opStr, key)
-                continue
-
-            try:
-                combinedVal = val0 + val1
-                if doAvg:
-                    combinedVal /= 2.0
-            except Exception:
-                self.log.warning("Could not %s metadata %r: value %r and/or %r not numeric",
-                                 opStr, key, val0, val1)
-                continue
-
-            combinedMetadata.set(key, combinedVal)
-
-    def makeInitialPsf(self, exposure, fwhmPix=None):
-        """Initialise the detection procedure by setting the PSF and WCS.
-
-        Parameters
-        ----------
-        exposure : `lsst.afw.image.Exposure`
-            Exposure to process.
-
-        Returns
-        -------
-        psf : `Unknown`
-            PSF, WCS
-
-        AssertionError
-            Raised if any of the following occur:
-            - No exposure provided.
-            - No wcs in exposure.
-        """
-        assert exposure, "No exposure provided"
-        wcs = exposure.getWcs()
-        assert wcs, "No wcs in exposure"
-
-        if fwhmPix is None:
-            fwhmPix = self.config.initialPsf.fwhm / wcs.getPixelScale().asArcseconds()
-
-        size = self.config.initialPsf.size
-        model = self.config.initialPsf.model
-        self.log.info("Initial snap PSF fwhm=%s pixels; size=%s pixels", fwhmPix, size)
-        psfCls = getattr(measAlg, model + "Psf")
-        psf = psfCls(size, size, fwhmPix/(2.0*num.sqrt(2*num.log(2.0))))
-        return psf
--- tests/test_snapCombine.py
+++ tests/test_snapCombine.py
@@ -80,8 +80,6 @@ class SnapCombineTestCase(lsst.utils.tests.TestCase):
         """Test addition with bad pixels
         """
         config = SnapCombineTask.ConfigClass()
-        config.doRepair = False
-        config.doDiffIm = False
         config.badMaskPlanes = ("BAD", "SAT", "NO_DATA", "CR")
         badPixelMask = afwImage.MaskX.getPlaneBitMask(config.badMaskPlanes)
         task = SnapCombineTask(config=config)
@@ -99,8 +97,6 @@ class SnapCombineTestCase(lsst.utils.tests.TestCase):
         """Test the case where all pixels are valid
         """
         config = SnapCombineTask.ConfigClass()
-        config.doRepair = False
-        config.doDiffIm = False
         task = SnapCombineTask(config=config)
 
         snap0 = makeRandomExposure(25, 25, 10000, 5000, 0)
@@ -112,61 +108,6 @@ class SnapCombineTestCase(lsst.utils.tests.TestCase):
         predMi += snap1.maskedImage
         self.assertMaskedImagesAlmostEqual(resMi, predMi)
 
-    def testMetadata(self):
-        """Test more advanced metadata handling
-        """
-        config = SnapCombineTask.ConfigClass()
-        config.doRepair = False
-        config.doDiffIm = False
-        # the MISS<N> keys are missing from metadata<N>
-        # and so cannot be summed or averaged
-        # MISS0 keys will be copied without alterate
-        # MISS1 keys will be missing from the result
-        config.averageKeys = ("AVG0", "AVG1", "MISS0AVG", "MISS1AVG")
-        config.sumKeys = ("SUM0", "SUM1", "MISS0SUM", "MISS1SUM")
-        task = SnapCombineTask(config=config)
-
-        snap0 = makeRandomExposure(5, 5, 10000, 5000, 0)
-        snap1 = makeRandomExposure(5, 5, 10000, 5000, 0)
-
-        metadata0 = snap0.getMetadata()
-        metadata1 = snap1.getMetadata()
-        metadata0.set("NUM0", 45.2)
-        metadata0.set("ASTR", "this is a string")
-        metadata0.set("AVG0", 10.5)
-        metadata1.set("AVG0", 9.5)
-        metadata0.set("AVG1", -0.7)
-        metadata1.set("AVG1", 0.2)
-        metadata0.set("MISS0AVG", 2.3523)
-        metadata1.set("MISS1AVG", 8.23)
-        metadata0.set("SUM0", 1.23)
-        metadata1.set("SUM0", 4.56)
-        metadata0.set("SUM1", 9814)
-        metadata1.set("SUM1", 3)
-        metadata0.set("MISS0SUM", 75.4)
-        metadata1.set("MISS1SUM", -234.3)
-
-        allKeys = set(metadata0.names()) | set(metadata1.names())
-        miss0Keys = set(key for key in allKeys if key.startswith("MISS0"))
-        miss1Keys = set(key for key in allKeys if key.startswith("MISS1"))
-        missKeys = miss0Keys | miss1Keys
-        avgKeys = set(config.averageKeys) - missKeys  # keys that will be averaged
-        sumKeys = set(config.sumKeys) - missKeys  # keys that will be summed
-        sameKeys = allKeys - (avgKeys | sumKeys | miss1Keys)  # keys that will be the same
-
-        resExp = task.run(snap0, snap1).exposure
-        resMetadata = resExp.getMetadata()
-
-        for key in sameKeys:
-            self.assertEqual(resMetadata.getScalar(key), metadata0.getScalar(key))
-        for key in avgKeys:
-            self.assertAlmostEqual(resMetadata.getScalar(key),
-                                   (metadata0.getScalar(key) + metadata1.getScalar(key)) / 2.0)
-        for key in sumKeys:
-            self.assertAlmostEqual(resMetadata.getScalar(key),
-                                   metadata0.getScalar(key) + metadata1.getScalar(key))
-        for key in miss1Keys:
-            self.assertFalse(resMetadata.exists(key))
 
 
 class MyMemoryTestCase(lsst.utils.tests.MemoryTestCase):
