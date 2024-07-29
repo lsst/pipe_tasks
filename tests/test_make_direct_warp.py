67f74ed4e9760e01ad5c19321116e3be9018f79a
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2024-07-29 12:04:31 -0400

    Override default configs in tests to prevent bit rot

--- tests/test_make_direct_warp.py
+++ tests/test_make_direct_warp.py
@@ -44,6 +44,8 @@ class MakeWarpTestCase(lsst.utils.tests.TestCase):
         np.random.seed(12345)
 
         self.config = MakeDirectWarpConfig()
+        self.config.doWarpMaskedFraction = True
+        self.config.numberOfNoiseRealizations = 1
 
         meanCalibration = 1e-4
         calibrationErr = 1e-5
