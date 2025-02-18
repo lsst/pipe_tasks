0465a711a0f124c1a40219c6bd3aeb028f23f3fb
gerenjie <jkurla@uw.edu>
2025-02-17 21:09:33 -0800

    run method args

--- python/lsst/pipe/tasks/drpAssociationPipe.py
+++ python/lsst/pipe/tasks/drpAssociationPipe.py
@@ -168,7 +168,9 @@ class DrpAssociationPipeTask(pipeBase.PipelineTask):
         inputs["tractId"] = butlerQC.quantum.dataId["tract"]
         inputs["patchId"] = butlerQC.quantum.dataId["patch"]
         inputs["idGenerator"] = self.config.idGenerator.apply(butlerQC.quantum.dataId)
-
+        if not self.config.doSolarSystemAssociation:
+            inputs["ssObjectTableRefs"] = []
+            inputs["finalVisitSummaryRefs"] = []
         outputs = self.run(**inputs)
         butlerQC.put(outputs, outputRefs)
 
