d2fc9645a598e61915022e98cd02124a19a0f112
Eli Rykoff <erykoff@stanford.edu>
2026-08-03 10:23:49 -0700

    Add comment explaining why the TEX values are in the visit table.

--- python/lsst/pipe/tasks/postprocess.py
+++ python/lsst/pipe/tasks/postprocess.py
@@ -1914,6 +1914,9 @@ class MakeVisitTableTask(pipeBase.PipelineTask):
             visitEntry["obsStart"] = visitEntry["expMidpt"] - 0.5 * np.timedelta64(int(expTime * 1E9), "ns")
             expTime_days = expTime / (60*60*24)
             visitEntry["obsStartMJD"] = visitEntry["expMidptMJD"] - 0.5 * expTime_days
+
+            # Note that these are computed per-visit and stored identically
+            # in each row, so we can use the first row values.
             visitEntry["psfTE1e1"] = visitRow["psfTE1e1"]
             visitEntry["psfTE1e2"] = visitRow["psfTE1e2"]
             visitEntry["psfTE1ex"] = visitRow["psfTE1ex"]
@@ -1926,6 +1929,7 @@ class MakeVisitTableTask(pipeBase.PipelineTask):
             visitEntry["psfTE4e1"] = visitRow["psfTE4e1"]
             visitEntry["psfTE4e2"] = visitRow["psfTE4e2"]
             visitEntry["psfTE4ex"] = visitRow["psfTE4ex"]
+
             visitEntries.append(visitEntry)
 
             # TODO: DM-30623, Add programId, exposureType, cameraTemp,
